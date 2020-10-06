7e1486d172a925c11aadf69fa38fe9d923db8b64
Florian Scherf <f.scherf@pengutronix.de>
2020-10-06 19:31:47 +0200

    views: rename View to ViewRuntime; move ViewRuntime to its own module
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- lona/request.py
+++ lona/request.py
@@ -8,13 +8,13 @@ class Client:
                 'operation is not supported in multi user requests')
 
     def _assert_view_is_interactive(self):
-        if not self.request._view.route.interactive:
+        if not self.request._view_runtime.route.interactive:
             raise RuntimeError(
                 'operation is not supported in non-interactive requests')
 
     def _assert_view_is_running(self):
-        if self.request._view.shutdown_error_class:
-            raise self.request._view.shutdown_error_class()
+        if self.request._view_runtime.shutdown_error_class:
+            raise self.request._view_runtime.shutdown_error_class()
 
     def _await_specific_user_input(self, *nodes, html=None, event_type=''):
         self._assert_single_user_request()
@@ -27,7 +27,7 @@ class Client:
         if len(nodes) == 1 and isinstance(nodes[0], list):
             nodes = nodes[0]
 
-        return self.request._view.await_user_input(
+        return self.request._view_runtime.await_user_input(
             html=html,
             event_type=event_type,
             nodes=nodes,
@@ -44,7 +44,7 @@ class Client:
         if self.request._multi_user:
             patch_input_events = False
 
-        self.request._view.send_data(
+        self.request._view_runtime.send_data(
             html=html,
             patch_input_events=patch_input_events,
         )
@@ -52,14 +52,14 @@ class Client:
     def set_title(self, title):
         self._assert_view_is_running()
 
-        self.request._view.send_data(title=title)
+        self.request._view_runtime.send_data(title=title)
 
     def await_user_input(self, html=None):
         self._assert_single_user_request()
         self._assert_view_is_interactive()
         self._assert_view_is_running()
 
-        return self.request._view.await_user_input(html=html)
+        return self.request._view_runtime.await_user_input(html=html)
 
     def await_click(self, *clickable_nodes, html=None):
         return self. _await_specific_user_input(
@@ -95,19 +95,19 @@ class View:
     def daemonize(self):
         self._assert_single_user_request()
 
-        self.request._view.is_daemon = True
+        self.request._view_runtime.is_daemon = True
 
 
 class Request:
-    def __init__(self, view, connection, post_data, multi_user=False):
-        self._view = view
+    def __init__(self, view_runtime, connection, post_data, multi_user=False):
+        self._view_runtime = view_runtime
         self._multi_user = multi_user
         self.connection = connection
 
-        self.url = self._view.url
+        self.url = self._view_runtime.url
 
         if self.url:
-            self.GET = dict(self._view.url.query)
+            self.GET = dict(self._view_runtime.url.query)
             self.POST = post_data or {}
 
         else:
@@ -116,9 +116,9 @@ class Request:
 
         self.method = 'POST' if self.POST else 'GET'
 
-        self.server = self._view.server
-        self.route = self._view.route
-        self.match_info = self._view.match_info
+        self.server = self._view_runtime.server
+        self.route = self._view_runtime.route
+        self.match_info = self._view_runtime.match_info
 
         self.client = Client(self)
         self.view = View(self)
@@ -128,7 +128,7 @@ class Request:
         if self._multi_user:
             user = []
 
-            for connection in self._view.connections.keys():
+            for connection in self._view_runtime.connections.keys():
                 user.append(connection.user)
 
             return user
