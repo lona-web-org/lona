f399f53e586b3719721458c01d779e2dad67de53
Florian Scherf <f.scherf@pengutronix.de>
2020-11-30 19:12:53 +0100

    connection: ignore ConnectionResetErrors
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- lona/connection.py
+++ lona/connection.py
@@ -13,14 +13,20 @@ class Connection:
         return self.websocket is not None
 
     def send_str(self, string):
-        # TODO: error handling (disconnects, reconnects, etc.)
         # TODO: find right priority
 
         if not self.is_interactive:
             raise NotInteractiveError
 
-        self.server.schedule(
-            self.websocket.send_str(string),
-            sync=True,
-            priority=self.server.settings.DEFAULT_VIEW_PRIORITY,
-        )
+        try:
+            self.server.schedule(
+                self.websocket.send_str(string),
+                sync=True,
+                priority=self.server.settings.DEFAULT_VIEW_PRIORITY,
+            )
+
+        except ConnectionResetError:
+            # this exception gets handled by aiohttp internally and
+            # can be ignored
+
+            pass
