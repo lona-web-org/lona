ab3ce9fa730c953500c29d167fc1719c6758f252
Florian Scherf <f.scherf@pengutronix.de>
2020-12-08 13:43:30 +0100

    logging: move log filtering into a python logging filter
    This solves all previous issues with formatting and empty lines
    
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- lona/logging.py
+++ lona/logging.py
@@ -6,20 +6,31 @@ import logging
 from lona.scheduling import get_current_thread_name
 
 
-class LogFormatter(logging.Formatter):
+class LogFilter(logging.Filter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.excluded = []
         self.included = []
 
-    def format(self, record):
+    def include(self, logger_name):
+        self.included.append(logger_name)
+
+    def exclude(self, logger_name):
+        self.excluded.append(logger_name)
+
+    def filter(self, record):
         if record.name in self.excluded:
-            return ''
+            return False
 
         if self.included and record.name not in self.included:
-            return ''
+            return False
 
+        return True
+
+
+class LogFormatter(logging.Formatter):
+    def format(self, record):
         # FIXME: only in debug mode
         thread_name = get_current_thread_name()
 
@@ -95,9 +106,3 @@ class LogFormatter(logging.Formatter):
             background,
             record_string,
         )
-
-    def include(self, logger_name):
-        self.included.append(logger_name)
-
-    def exclude(self, logger_name):
-        self.excluded.append(logger_name)
