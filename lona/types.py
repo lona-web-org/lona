4887968f11615ee3b2668b84fd04c407f8a9a466
Florian Scherf <f.scherf@pengutronix.de>
2020-12-11 13:07:09 +0100

    types: Symbol: add better comparisons
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- lona/types.py
+++ lona/types.py
@@ -13,18 +13,6 @@ class Symbol:
         else:
             self.value = self.name
 
-    def __eq__(self, other):
-        other_value = other
-
-        if isinstance(other_value, Symbol):
-            other_value = other_value.value
-
-        if isinstance(other_value, str):
-            other_value = other_value.strip().upper()
-
-        return (other_value == self.name or
-                other_value == self.value)
-
     def __repr__(self):
         return self.name
 
@@ -51,6 +39,37 @@ class Symbol:
                 data[class_name][attribute.name] = attribute.value
 
         return data
+
+    # comparisons #############################################################
+    def __eq__(self, other):
+        other_value = other
+
+        if isinstance(other_value, Symbol):
+            other_value = other_value.value
+
+        if isinstance(other_value, str):
+            other_value = other_value.strip().upper()
+
+        return (other_value == self.name or
+                other_value == self.value)
+
+    def _compare(self, other, operator):
+        if isinstance(other, Symbol):
+            return operator(self.value, other.value)
+
+        return operator(self.value, other)
+
+    def __lt__(self, other):
+        return self._compare(other, lambda a, b: a < b)
+
+    def __lte__(self, other):
+        return self._compare(other, lambda a, b: a <= b)
+
+    def __gt__(self, other):
+        return self._compare(other, lambda a, b: a > b)
+
+    def __gte__(self, other):
+        return self._compare(other, lambda a, b: a >= b)
 
 
 class Mapping:
