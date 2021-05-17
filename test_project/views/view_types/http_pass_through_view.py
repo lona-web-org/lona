4d8b2cc770d0861bf0cbc2838110073c07eea79d
Florian Scherf <f.scherf@pengutronix.de>
2021-05-17 11:40:15 +0200

    test project: view types: http pass through: add home link
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- test_project/views/view_types/http_pass_through_view.py
+++ test_project/views/view_types/http_pass_through_view.py
@@ -6,6 +6,9 @@ from aiohttp.web import Response
 class HTTPPassThroughView(LonaView):
     def handle_request(self, request):
         return Response(
-            body='<h2>Pass Through View</h2>',
+            body="""
+                <h2>Pass Through View</h2>
+                <a href="/">Home</a>
+            """,
             content_type='text/html',
         )
