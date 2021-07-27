c74d048805ea8ceb7bf92100cf80895fa70ddbb7
Florian Scherf <f.scherf@pengutronix.de>
2021-07-27 12:53:37 +0200

    contrib: django: auth: add permission checks
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- test_project/views/django/permission_views.py
+++ test_project/views/django/permission_views.py
@@ -1,11 +1,8 @@
-from lona.errors import ForbiddenError
 from lona.view import LonaView
 
 
 class DjangLoginView(LonaView):
-    def handle_user_enter(self, request):
-        if not (request.user.is_active and request.user.is_authenticated):
-            raise ForbiddenError
+    DJANGO_AUTH_LOGIN_REQUIRED = True
 
     def handle_request(self, request):
         return '<h2>Hello {}!</h2>'.format(request.user)
