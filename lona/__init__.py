14e5c1e1148324b3288111fffa0ec758b37fa35d
Florian Scherf <f.scherf@pengutronix.de>
2021-08-13 08:23:44 +0200

    add import shortcuts
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- lona/__init__.py
+++ lona/__init__.py
@@ -1,4 +1,14 @@
-from .app import LonaApp  # NOQA
+try:
+    from .routing import Route, MATCH_ALL  # NOQA
+    from .view import LonaView  # NOQA
+    from .app import LonaApp  # NOQA
+    from .exceptions import *  # NOQA
+    from .errors import *  # NOQA
+
+except ImportError:
+    # this happens while packaging and can be ignored
+
+    pass
 
 VERSION = (1, 0, 2)
 VERSION_STRING = '{}'.format('.'.join([str(i) for i in VERSION]))
