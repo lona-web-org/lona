250a6d644f69b0d75dafa20ebf2ce7a1e5d9e236
Florian Scherf <f.scherf@pengutronix.de>
2021-08-25 09:48:15 +0200

    test project: settings: remove old Django artifacts
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- test_project/settings.py
+++ test_project/settings.py
@@ -1,5 +1,3 @@
-import os
-
 MAX_WORKER_THREADS = 4
 MAX_RUNTIME_THREADS = 3
 MAX_APPLICATION_THREADS = 0
@@ -25,11 +23,3 @@ MIDDLEWARES = [
 
 ERROR_404_VIEW = 'views/error_404.py::Error404View'
 ERROR_500_VIEW = 'views/error_500.py::Error500View'
-
-if os.environ.get('DJANGO', '0') == '1':
-    MIDDLEWARES += [
-        'lona.contrib.django.auth.DjangoSessionMiddleware',
-        'middlewares.py::RateLimitMiddleware',
-    ]
-
-    ERROR_500_VIEW = 'views/django/error_500.py::Error500View'
