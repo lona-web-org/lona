18de668095ca66d5a0fda1dce03588a4f60650f8
Marat Reymers <marat.maratori@gmail.com>
2021-09-26 00:36:24 +0300

    refactor(static files): use @dataclass for static files
    Signed-off-by: Marat Reymers <marat.maratori@gmail.com>

--- lona/static_files.py
+++ lona/static_files.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import Enum
 
 
@@ -7,32 +8,19 @@ class SORT_ORDER(Enum):
     APPLICATION = 300
 
 
+@dataclass
 class StaticFile:
-    def __init__(self, name, path, url=None, sort_order=None, link=True,
-                 enabled_by_default=True):
+    name: str
+    path: str
+    url: str = ''
+    sort_order: SORT_ORDER = SORT_ORDER.APPLICATION
+    link: bool = True
+    enabled_by_default: bool = True
 
-        self.name = name
-        self.path = path
-        self.url = url
-        self.sort_order = sort_order or SORT_ORDER.APPLICATION
-        self.link = link
-        self.enabled_by_default = enabled_by_default
-        self.enabled = True
-
-        # this value is only for the object representation
-        # and is meant for debugging
-        self.static_url_prefix = ''
-
-    def __repr__(self):
-        return '<{}({}, sort_order={}, enabled={}, {}{} -> {})>'.format(  # NOQA: FS002
-            self.__class__.__name__,
-            self.name,
-            repr(self.sort_order),
-            repr(self.enabled),
-            self.static_url_prefix,
-            self.url,
-            self.path,
-        )
+    # this is only for internal use
+    enabled: bool = True
+    # this is only for the object representation and is meant for debugging
+    static_url_prefix: str = ''
 
 
 class StyleSheet(StaticFile):
