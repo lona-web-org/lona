lona-web-org/lona

commit 885845e4e94533b952b79456fa67758a4f293fc7

feat(views): add LonaView.on_stop hook
Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

lona/view_runtime.py
@@ -243,13 +243,34 @@ class ViewRuntime:
             self.stop_reason,
         )
 
+        stop_reason = self.stop_reason
+
+        if not isinstance(stop_reason, (ServerStop, CancelledError)):
+            stop_reason = None
+
         try:
-            self.view.on_shutdown(self.stop_reason)
+            self.view.on_shutdown(stop_reason)
 
         except Exception:
             logger.exception(
                 'Exception raised while running %s',
                 self.view.on_shutdown,
+            )
+
+    def run_stop_hook(self):
+        logger.debug(
+            'running %s with stop reason %s',
+            self.view.on_stop,
+            self.stop_reason,
+        )
+
+        try:
+            self.view.on_stop(self.stop_reason)
+
+        except Exception:
+            logger.exception(
+                'Exception raised while running %s',
+                self.view.on_stop,
             )
 
     def start(self):
@@ -295,11 +316,10 @@ class ViewRuntime:
 
             return self.handle_raw_response_dict(raw_response_dict)
 
-        except(StopReason, CancelledError) as _stop_reason:
-            self.stop_reason = _stop_reason
-
         # 403 Forbidden
         except ForbiddenError as exception:
+            self.stop_reason = exception
+
             view_class = self.server.view_loader.load(
                 self.server.settings.CORE_ERROR_403_VIEW,
             )
@@ -344,6 +364,7 @@ class ViewRuntime:
             self.is_stopped = True
             self.stopped_at = datetime.now()
             self.send_view_stop()
+            self.run_stop_hook()
 
         self.run_shutdown_hook()
 

lona/view.py
@@ -348,3 +348,6 @@ class LonaView:
             ],
     ) -> None:
         pass
+
+    def on_stop(self, reason: Exception | None) -> None:
+        pass

lona/view_loader.py
@@ -46,6 +46,7 @@ class ViewLoader:
             'handle_input_event',
             'on_view_event',
             'on_shutdown',
+            'on_stop',
         ]
 
         for hook_name in hook_names:

commit 451ab59fdd28ba29d884c295d31ee45780d4e183

fix(views): fix handling of lona.exceptions.StopReason
This broke in 885845e4e94533b952b79456fa67758a4f293fc7.

Because lona.exceptions.StopReason gets handled like every other exception,
Lona produces a error log line on every user disconnect, which makes no sense
and is confusing to end users.

Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

lona/view_runtime.py
@@ -344,6 +344,10 @@ class ViewRuntime:
 
             return self.handle_raw_response_dict(raw_response_dict)
 
+        # view got stopped from outside
+        except(StopReason, CancelledError) as exception:
+            self.stop_reason = exception
+
         # 403 Forbidden
         except ForbiddenError as exception:
             self.stop_reason = exception