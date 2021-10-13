lona-web-org/lona

commit e7e70dbd367016eb9b43b06ef258a14982780e48

LonaView: deprecate on_shutdown
Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

lona/view_loader.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import Type, cast
+import warnings
 import logging
 import inspect
 import asyncio
@@ -61,6 +62,13 @@ class ViewLoader:
                     route.view,
                     hook_name,
                 )
+
+        # TODO: remove after 1.8
+        if view.on_shutdown is not LonaView.on_shutdown:
+            warnings.warn(
+                'LonaView.on_shutdown() will be removed in 1.8',
+                category=DeprecationWarning,
+            )
 
     def _generate_acquiring_error_view(
             self,

lona/view.py
@@ -347,6 +347,8 @@ class LonaView:
                 Exception,
             ],
     ) -> None:
+        # TODO: remove after 1.8
+
         pass
 
     def on_stop(self, reason: Exception | None) -> None:

lona/view_runtime.py
@@ -237,6 +237,8 @@ class ViewRuntime:
 
     # start and stop ##########################################################
     def run_shutdown_hook(self):
+        # TODO: remove after 1.8
+
         logger.debug(
             'running %s with stop reason %s',
             self.view.on_shutdown,

commit 78d7335aff3bf78e0156b5821b2dd2c2654db05b

fix(views): ViewLoader: fix handling of HTTP pass through views
This broke in e7e70dbd367016eb9b43b06ef258a14982780e48.

HTTP pass through views are simple callbacks that will have no attribute
"on_shutdown".

Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

lona/view_loader.py
@@ -67,7 +67,9 @@ class ViewLoader:
                 )
 
         # TODO: remove after 1.8
-        if view.on_shutdown is not LonaView.on_shutdown:
+        if(isinstance(view, type) and
+           view.on_shutdown is not LonaView.on_shutdown):
+
             warnings.warn(
                 'LonaView.on_shutdown() will be removed in 1.8',
                 category=DeprecationWarning,
@@ -105,7 +107,10 @@ class ViewLoader:
     ) -> None:
 
         view_class = self._acquire(view)
-        view_class._server = self.server  # TODO: remove after 1.8
+
+        # TODO: remove after 1.8
+        if isinstance(view_class, type) and issubclass(view_class, LonaView):
+            view_class._server = self.server
 
         if route:
             self._run_checks(route, view_class)