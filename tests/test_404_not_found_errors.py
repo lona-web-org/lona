10b614fc5bcba863f0e565c37b59e7920bcdfed8
Florian Scherf <f.scherf@pengutronix.de>
2021-11-22 18:06:00 +0100

    tests(error views): add tests for 403, 404 and 500 errors
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

---
+++ tests/test_404_not_found_errors.py
@@ -0,0 +1,40 @@
+from lona import NotFoundError, LonaView
+
+
+def setup_app(app):
+
+    @app.error_404_view
+    class Error404NotFoundView(LonaView):
+        def handle_request(self, request):
+            return 'NOT FOUND'
+
+    @app.route('/url/<name>')
+    class View2(LonaView):
+        def handle_request(self, request):
+            if request.match_info['name'] == 'foo':
+                raise NotFoundError
+
+            return request.match_info['name']
+
+
+async def test_404_not_found_errors(lona_app_context):
+    from playwright.async_api import async_playwright
+
+    context = await lona_app_context(setup_app)
+
+    async with async_playwright() as playwright:
+        context.server.state['view1'] = []
+        context.server.state['view2'] = []
+
+        browser = await playwright.chromium.launch()
+        browser_context = await browser.new_context()
+        page = await browser_context.new_page()
+
+        await page.goto(context.make_url('/foo'))
+        await page.wait_for_selector('body:has-text("NOT FOUND")')
+
+        await page.goto(context.make_url('/url/foo'))
+        await page.wait_for_selector('#lona:has-text("NOT FOUND")')
+
+        await page.goto(context.make_url('/url/bar'))
+        await page.wait_for_selector('#lona:has-text("bar")')
---
+++ tests/test_500_internal_errors.py
@@ -0,0 +1,37 @@
+from lona import LonaView
+
+
+def setup_app(app):
+
+    @app.error_500_view
+    class Error500InternalErrorView(LonaView):
+        def handle_request(self, request):
+            return 'INTERNAL ERROR'
+
+    @app.route('/url/<name>')
+    class View2(LonaView):
+        def handle_request(self, request):
+            if request.match_info['name'] == 'foo':
+                raise RuntimeError
+
+            return request.match_info['name']
+
+
+async def test_500_internal_errors(lona_app_context):
+    from playwright.async_api import async_playwright
+
+    context = await lona_app_context(setup_app)
+
+    async with async_playwright() as playwright:
+        context.server.state['view1'] = []
+        context.server.state['view2'] = []
+
+        browser = await playwright.chromium.launch()
+        browser_context = await browser.new_context()
+        page = await browser_context.new_page()
+
+        await page.goto(context.make_url('/url/foo'))
+        await page.wait_for_selector('#lona:has-text("INTERNAL ERROR")')
+
+        await page.goto(context.make_url('/url/bar'))
+        await page.wait_for_selector('#lona:has-text("bar")')
