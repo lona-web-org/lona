bc5a8ffe73750deca37ed7c6eb4a4d68182b3cf1
Florian Scherf <f.scherf@pengutronix.de>
2021-12-11 17:39:50 +0100

    fix(dependencies): handle import errors while importing lona properly
    Previously all import errors while packaging, testing or linting were ignored,
    which covered up real import errors in production.
    
    This patch adds all runtime dependencies to the packaging, testing and linting
    dependencies, and removes the code that previously handled and ignored import
    errors.
    
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- lona/__init__.py
+++ lona/__init__.py
@@ -1,14 +1,8 @@
-try:
-    from .exceptions import *  # NOQA: F403
-    from .routing import MATCH_ALL, Route
-    from .errors import *  # NOQA: F403
-    from .view import LonaView
-    from .app import LonaApp
-
-except ImportError:
-    # this happens while packaging and can be ignored
-
-    pass
+from .exceptions import *  # NOQA: F403
+from .routing import MATCH_ALL, Route
+from .errors import *  # NOQA: F403
+from .view import LonaView
+from .app import LonaApp
 
 VERSION = (1, 8, 4)
 VERSION_STRING = '.'.join(str(i) for i in VERSION)
