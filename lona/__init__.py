2bf2b13f41d612b1e0594dd4b486ce9230d9af11
Florian Scherf <f.scherf@pengutronix.de>
2022-01-06 15:06:13 +0100

    fix(packaging): fix import error while installing
    Previously an import error stating that typing-extensions is not installed
    could occur while installing the Lona package
    
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- lona/__init__.py
+++ lona/__init__.py
@@ -1,8 +1,14 @@
-from .exceptions import *  # NOQA: F403
-from .routing import MATCH_ALL, Route
-from .errors import *  # NOQA: F403
-from .view import LonaView
-from .app import LonaApp
+try:
+    from .exceptions import *  # NOQA: F403
+    from .routing import MATCH_ALL, Route
+    from .errors import *  # NOQA: F403
+    from .view import LonaView
+    from .app import LonaApp
+
+except ImportError as e:
+    # this can happen while installing the package and can be ignored
+    if e.name != 'typing_extensions':
+        raise
 
 VERSION = (1, 8, 5)
 VERSION_STRING = '.'.join(str(i) for i in VERSION)
