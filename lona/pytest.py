67d3999181c16d7999c91224d2e63bca25a8fab9
Florian Scherf <mail@florianscherf.de>
2022-01-25 15:46:33 +0100

    pytest: add debug interactive
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- lona/pytest.py
+++ lona/pytest.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
 from collections.abc import AsyncIterator, Iterator, Callable
-from typing import AsyncContextManager, cast
-from asyncio import AbstractEventLoop, sleep
+from typing import AsyncContextManager, Dict, cast, Any
+from asyncio import AbstractEventLoop, Future, sleep
 from contextlib import asynccontextmanager
 from dataclasses import dataclass
+import webbrowser as _webbrowser
 import time
+import os
 
 from aiohttp.test_utils import TestClient
 from aiohttp.web import Application
 import pytest
 
+from lona.shell.shell import embed_shell
 from lona.worker_pool import WorkerPool
 from lona.server import LonaServer
 from lona import LonaApp
@@ -21,13 +24,81 @@ class LonaContext:
     client: TestClient
     app: None | LonaApp
     server: LonaServer
+    loop: AbstractEventLoop
 
     def make_url(self, path: str = '') -> str:
         return str(self.client.make_url(path))
 
+    def debug_interactive(
+            self,
+            webbrowser: bool = True,
+            sync: bool = False,
+            locals: Dict[str, Any] | None = None,
+    ) -> Future | None:  # pragma: no cover
+
+        """
+        This method pauses the current test by starting an rlpython shell and
+        starts a webbrowser that points to the currently running test server.
+        The test continues when the rlpython shell is exited.
+
+        Async Example:
+
+            async def test_my_feature(lona_app_context):
+                context = await lona_app_context(setup_app)
+
+                await context.debug_interactive(locals=locals())
+
+        Sync Example:
+
+            async def test_my_feature(lona_app_context):
+                context = await lona_app_context(setup_app)
+
+                def run_test():
+                    context.debug_interactive(
+                        sync=True,
+                        locals=locals(),
+                    )
+
+                context.loop.run_in_executor(None, run_test)
+
+        :webbrowser: (bool) start a webbrowser that points to the test server
+        :sync:       (bool) run blocking in the current thread
+        :locals:     (dict|None) variable overrides for the rlpython shell
+        """
+
+        def _debug_interactive():
+            # start browser
+            if webbrowser:
+                os.environ['DISPLAY'] = ':0'
+                _webbrowser.open(self.make_url())
+
+            # embed shell
+            _locals = locals or {}
+
+            _locals = {
+                'server': self.server,
+                'lona_context': self,
+                **_locals,
+            }
+
+            embed_shell(
+                server=self.server,
+                locals=_locals,
+            )
+
+        if sync:
+            _debug_interactive()
+
+            return None
+
+        return cast(
+            Future,
+            self.loop.run_in_executor(None, _debug_interactive),
+        )
+
 
 @pytest.fixture()
-def lona_app_context(request, aiohttp_client):
+def lona_app_context(request, aiohttp_client, loop):
     async def setup_lona_app_context(
             setup_app: Callable[[LonaApp], None],
             project_root: str = '',
@@ -48,13 +119,14 @@ def lona_app_context(request, aiohttp_client):
             client=client,
             app=lona_app,
             server=cast(LonaServer, lona_app.server),
+            loop=loop,
         )
 
     return setup_lona_app_context
 
 
 @pytest.fixture()
-def lona_project_context(request, aiohttp_client):
+def lona_project_context(request, aiohttp_client, loop):
     async def setup_lona_project_context(
         project_root: str = '',
         settings: None | list[str] = None,
@@ -90,6 +162,7 @@ def lona_project_context(request, aiohttp_client):
             client=client,
             app=None,
             server=cast(LonaServer, server),
+            loop=loop,
         )
 
     return setup_lona_project_context
