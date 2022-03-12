lona-web-org/lona

commit ff7a0ab8316c9a4faf96e939d656fc52e66a514d

fix(views): fix page titles on daemonized views
Previously the message for changing the page title using View.show() or
View.set_title() was send only once. This is fine for non-daemon views but on
daemon views the title has to be resend everytime the user reconnects, or a
second user connects.

This patch adds a title field to the HTML document, so it gets saved beyond
reconnects and adds title resends to the view runtime.

Signed-off-by: Florian Scherf <mail@florianscherf.de>

lona/html/document.py
@@ -7,8 +7,9 @@ from lona.protocol import DATA_TYPE
 
 class Document:
     def __init__(self):
-        self._lock = RLock()
+        self.title = ''
         self.html = None
+        self._lock = RLock()
         self._patch_stack = PatchStack()
 
     @property
@@ -51,21 +52,29 @@ class Document:
         if not self.html:
             return self.apply('')
 
-        return DATA_TYPE.HTML_TREE, self.html._serialize()
+        # HTML string
+        if isinstance(self.html, str):
+            return self.title, DATA_TYPE.HTML, self.html
 
-    def apply(self, html):
-        if isinstance(html, str) and html is self.html:
-            return
+        # HTML tree
+        return self.title, DATA_TYPE.HTML_TREE, self.html._serialize()
+
+    def apply(self, title='', html=None):
+        if title:
+            self.title = title
+
+        if html is None:
+            return self.title, None, None
 
         # HTML update
         elif html is self.html:
             if not self._patch_stack.has_patches():
-                return
+                return self.title, None, None
 
             patches = self._patch_stack.get_patches()
             self._patch_stack.clear()
 
-            return DATA_TYPE.HTML_UPDATE, patches
+            return self.title, DATA_TYPE.HTML_UPDATE, patches
 
         # HTML
         else:
@@ -80,9 +89,9 @@ class Document:
 
                 self.html._set_document(self)
 
-                return self.serialize()
+                return self.title, DATA_TYPE.HTML_TREE, self.html._serialize()
 
             # HTML string
             self.html = str(html)
 
-            return DATA_TYPE.HTML, html
+            return self.title, DATA_TYPE.HTML, html

lona/view_runtime.py
@@ -437,12 +437,13 @@ class ViewRuntime:
         with self.document.lock:
             self.connections[connection] = (window_id, url)
 
-            data_type, data = self.document.serialize()
+            title, data_type, data = self.document.serialize()
 
-            if not data:
+            if not data_type or not data:
                 return
 
             self.send_data(
+                title=title,
                 data=[data_type, data],
                 connections={connection: (window_id, url)},
             )
@@ -457,12 +458,13 @@ class ViewRuntime:
                 },
             )
 
-            data_type, data = self.document.serialize()
+            title, data_type, data = self.document.serialize()
 
-            if not data:
+            if not data_type or not data:
                 return
 
             self.send_data(
+                title=title,
                 data=[data_type, data],
                 connections={connection: (window_id, url)},
             )
@@ -623,8 +625,15 @@ class ViewRuntime:
 
         elif response_dict['text']:
             with self.document.lock:
-                data = self.document.apply(response_dict['text'])
-                self.send_data(data=data, connections=connections)
+                title, data_type, data = self.document.apply(
+                    html=response_dict['text'],
+                )
+
+                self.send_data(
+                    title=title,
+                    data=[data_type, data],
+                    connections=connections,
+                )
 
         return response_dict
 
@@ -662,10 +671,15 @@ class ViewRuntime:
 
         def send_html_patches():
             with self.document.lock:
-                data = self.document.apply(self.document.html)
+                title, data_type, data = self.document.apply(
+                    self.document.html,
+                )
 
-                if data:
-                    self.send_data(data=data)
+                if data_type and data:
+                    self.send_data(
+                        title=title,
+                        data=[data_type, data],
+                    )
 
         def log_handled_message():
             input_events_logger.debug(

lona/view.py
@@ -133,15 +133,24 @@ class LonaView:
 
         with self._view_runtime.document.lock:
             html = html or self._view_runtime.document.html
-            data = self._view_runtime.document.apply(html)
 
-            if data:
-                self._view_runtime.send_data(data=data, title=title)
+            title, data_type, data = self._view_runtime.document.apply(
+                title=title,
+                html=html,
+            )
+
+            if data_type and data:
+                self._view_runtime.send_data(
+                    title=title,
+                    data=[data_type, data],
+                )
 
     def set_title(self, title: str) -> None:
         self._assert_not_main_thread()
         self._assert_view_is_interactive()
         self._assert_view_is_running()
+
+        title, _, _ = self._view_runtime.document.apply(title=title)
 
         with self._view_runtime.document.lock:
             self._view_runtime.send_data(title=title)

tests/test_title.py
@@ -1,0 +1,53 @@
+def setup_app(app):
+    from datetime import datetime
+
+    from lona import LonaView
+
+    @app.route('/')
+    class HoweView(LonaView):
+        def handle_request(self, request):
+            return 'HOME'
+
+    @app.route('/daemonized-view/')
+    class DaemonizedView(LonaView):
+        def handle_request(self, request):
+            self.daemonize()
+            self.set_title('Daemonized View')
+            self.show(f'View started: {datetime.now()}')
+
+            self.await_input_event()
+
+
+async def test_title(lona_app_context):
+    """
+    This test tests page titles on non-daemon views and daemon views
+    """
+
+    from playwright.async_api import async_playwright
+
+    context = await lona_app_context(setup_app)
+
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        browser_context = await browser.new_context()
+        page = await browser_context.new_page()
+
+        # test default title
+        await page.goto(context.make_url('/'))
+        await page.wait_for_selector('#lona:has-text("HOME")')
+        assert await page.title() == 'Lona'
+
+        # get timestamp of the daemonized view
+        await page.goto(context.make_url('/daemonized-view/'))
+        await page.wait_for_selector('#lona:has-text("View started: ")')
+        timestamp = await page.inner_text('#lona')
+
+        # test titles
+        assert await page.title() == 'Daemonized View'
+
+        # reload tab
+        await page.reload()
+        await page.wait_for_selector('#lona:has-text("View started: ")')
+
+        assert await page.inner_text('#lona') == timestamp
+        assert await page.title() == 'Daemonized View'

commit 0895a7846991c641431e82f83ac7c31d5d05f9ae

fix(HTML): Document: fix incorrect usage of Document.apply
This patch fixes a bug, introduced in

    ff7a0ab8316c ("fix(views): fix page titles on daemonized views")

ff7a0ab8316c introduced a new, optional argument to Document.apply, named
'title' and broke all code that calls Document.apply without named arguments.

Signed-off-by: Florian Scherf <mail@florianscherf.de>

lona/html/document.py
@@ -50,7 +50,7 @@ class Document:
 
     def serialize(self):
         if not self.html:
-            return self.apply('')
+            return self.apply(html='')
 
         # HTML string
         if isinstance(self.html, str):

lona/view_runtime.py
@@ -665,7 +665,7 @@ class ViewRuntime:
         def send_html_patches():
             with self.document.lock:
                 title, data_type, data = self.document.apply(
-                    self.document.html,
+                    html=self.document.html,
                 )
 
                 if data_type and data: