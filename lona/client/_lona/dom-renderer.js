bcf4d295f7fce93f2b4e61da83a0bb590e764a27
Florian Scherf <mail@florianscherf.de>
2022-04-26 07:52:00 +0200

    client: dom-renderer: remove obsolete _this
    The code previously used _this variables to account for scope losses in
    callback functions. This is unnecessary since Javascript supports arrow
    functions that inherit the variable scope of their parent.
    
    This patch removes all occurrences of _this and replaces them with arrow
    function code.
    
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- lona/client/_lona/dom-renderer.js
+++ lona/client/_lona/dom-renderer.js
@@ -4,7 +4,6 @@ Lona.LonaDomRenderer = function(lona_context, lona_window) {
 
     // html rendering ---------------------------------------------------------
     this._render_node = function(node_spec) {
-        var _this = this;
         var property_names = ['value', 'checked', 'selected'];
 
         var node_list = [];
@@ -27,7 +26,7 @@ Lona.LonaDomRenderer = function(lona_context, lona_window) {
 
             // id list
             if(node_id_list.length > 0) {
-                _this.lona_window._dom_updater._add_id(node, node_id_list);
+                this.lona_window._dom_updater._add_id(node, node_id_list);
             };
 
             // class list
@@ -37,14 +36,14 @@ Lona.LonaDomRenderer = function(lona_context, lona_window) {
 
             // style
             if(Object.keys(node_style).length > 0) {
-                Object.keys(node_style).forEach(function(key) {
+                Object.keys(node_style).forEach(key => {
                     node.style[key] = node_style[key];
                 });
             };
 
             // attributes
             if(Object.keys(node_attributes).length > 0) {
-                Object.keys(node_attributes).forEach(function(key) {
+                Object.keys(node_attributes).forEach(key => {
                     var value = node_attributes[key];
 
                     // properties
@@ -64,17 +63,17 @@ Lona.LonaDomRenderer = function(lona_context, lona_window) {
             };
 
             // nodes
-            node_child_nodes.forEach(function(sub_node_argspec) {
-                var sub_node_list = _this._render_node(
+            node_child_nodes.forEach(sub_node_argspec => {
+                var sub_node_list = this._render_node(
                     sub_node_argspec);
 
-                _this.lona_window._dom_updater._apply_node_list(
+                this.lona_window._dom_updater._apply_node_list(
                     node,
                     sub_node_list,
                 );
             });
 
-            _this.lona_window._nodes[node_id] = node;
+            this.lona_window._nodes[node_id] = node;
             node_list.push(node);
 
         // TextNode
@@ -84,7 +83,7 @@ Lona.LonaDomRenderer = function(lona_context, lona_window) {
 
             var node = document.createTextNode(node_content);
 
-            _this.lona_window._nodes[node_id] = node;
+            this.lona_window._nodes[node_id] = node;
             node_list.push(node);
 
         // Widget
@@ -101,13 +100,13 @@ Lona.LonaDomRenderer = function(lona_context, lona_window) {
             var end_marker = document.createComment(
                 'end-lona-widget:' + node_id);
 
-            _this.lona_window._widget_marker[node_id] = start_marker;
+            this.lona_window._widget_marker[node_id] = start_marker;
 
             node_list.push(start_marker);
 
             // nodes
-            node_child_nodes.forEach(function(sub_node_argspec) {
-                var sub_node_list = _this._render_node(
+            node_child_nodes.forEach(sub_node_argspec => {
+                var sub_node_list = this._render_node(
                     sub_node_argspec);
 
                 node_list.push(sub_node_list);
@@ -122,21 +121,21 @@ Lona.LonaDomRenderer = function(lona_context, lona_window) {
 
                 var window_shim = new Lona.LonaWindowShim(
                     lona_context,
-                    _this.lona_window,
+                    this.lona_window,
                     node_id,
                 );
 
                 var widget = new widget_class(window_shim);
 
-                _this.lona_window._widgets[node_id] = widget;
-                _this.lona_window._widget_data[node_id] = widget_data;
-                _this.lona_window._widgets_to_setup.splice(0, 0, node_id);
+                this.lona_window._widgets[node_id] = widget;
+                this.lona_window._widget_data[node_id] = widget_data;
+                this.lona_window._widgets_to_setup.splice(0, 0, node_id);
             };
         };
 
         // patch input events
-        node_list.forEach(function(node) {
-            _this.lona_window._input_event_handler.patch_input_events(node);
+        node_list.forEach(node => {
+            this.lona_window._input_event_handler.patch_input_events(node);
         });
 
         return node_list;
@@ -146,7 +145,6 @@ Lona.LonaDomRenderer = function(lona_context, lona_window) {
         // TODO: get rid of this method and move functionality
         // into _render_node()
 
-        var _this = this;
         var node_list = [];
 
         for(var index in node_specs) {
@@ -154,8 +152,8 @@ Lona.LonaDomRenderer = function(lona_context, lona_window) {
         };
 
         // patch input events
-        node_list.forEach(function(node) {
-            _this.lona_window._input_event_handler.patch_input_events(node);
+        node_list.forEach(node => {
+            this.lona_window._input_event_handler.patch_input_events(node);
         });
 
         return node_list;
