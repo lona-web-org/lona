lona-web-org/lona

commit 7d8b5513b72d7efd48625db02ac4f5f81b311fe5

client: remove client pre compiler
Since the client uses ES06 imports now, this component is obsolete

Signed-off-by: Florian Scherf <mail@florianscherf.de>

lona/static_file_loader.py
@@ -26,7 +26,6 @@ class StaticFileLoader:
         self.static_dirs: list[str] = [
             *self.server.settings.STATIC_DIRS,
             *self.server.settings.CORE_STATIC_DIRS,
-            self.server._client_pre_compiler.tmp_dir.name,
             CLIENT_ROOT,
         ]
 
@@ -61,9 +60,6 @@ class StaticFileLoader:
         self.script_tags_html: str = self.server.render_template(
             self.server.settings.STATIC_FILES_SCRIPT_TAGS_TEMPLATE,
             {
-                'client_source_files':
-                    self.server._client_pre_compiler.get_source_files(),
-
                 'scripts': [i for i in self.scripts
                             if i.enabled and i.link],
             },
@@ -214,14 +210,6 @@ class StaticFileLoader:
 
         if rel_path.startswith('/'):
             rel_path = rel_path[1:]
-
-        # javascript client
-        logger.debug('trying Lona Javascript client')
-
-        abs_path = self.server._client_pre_compiler.resolve_path(rel_path)
-
-        if abs_path:
-            return abs_path
 
         # searching in static dirs
         for static_dir in self.static_dirs:

lona/client_pre_compiler.py
@@ -1,131 +1,0 @@
-from __future__ import annotations
-
-from tempfile import TemporaryDirectory
-from typing import Dict, Any
-import logging
-import os
-
-from jinja2 import FileSystemLoader, Environment
-
-from lona.protocol import ENUMS
-from lona._json import dumps
-
-SOURCE_ROOT = os.path.join(os.path.dirname(__file__), 'client')
-
-SOURCE_FILES = [
-    '_lona/window-shim.js',
-    '_lona/dom-renderer.js',
-    '_lona/dom-updater.js',
-    '_lona/widget-data-updater.js',
-    '_lona/input-events.js',
-    '_lona/window.js',
-    '_lona/context.js',
-]
-
-logger = logging.getLogger('lona.client_pre_compiler')
-
-
-class ClientPreCompiler:
-    def __init__(self, server):
-        self.server = server
-
-        # setup temp dir
-        self.tmp_dir = TemporaryDirectory()
-
-        os.makedirs(os.path.join(self.tmp_dir.name, '_lona'))
-
-        # setup templating environment
-        self.jinja2_env = Environment(
-            loader=FileSystemLoader(SOURCE_ROOT),
-        )
-
-        # compile
-        if self.server.settings.CLIENT_DEBUG:
-            logger.warning('running in debug mode')
-
-        self.compile()
-
-    def get_settings(self) -> Dict[str, Any]:
-        settings = self.server.settings
-
-        return {
-            'DEBUG': settings.CLIENT_DEBUG,
-            'VIEW_START_TIMEOUT': settings.CLIENT_VIEW_START_TIMEOUT,
-            'INPUT_EVENT_TIMEOUT': settings.CLIENT_INPUT_EVENT_TIMEOUT,
-            'PING_INTERVAL': settings.CLIENT_PING_INTERVAL,
-        }
-
-    def get_enums(self) -> Dict[str, Dict[str, Any]]:
-        enums = {}
-
-        for enum in ENUMS:
-            enum_values = {}
-
-            for enum_value in enum:
-                enum_values[enum_value.name] = enum_value.value
-
-            enums[enum.__name__] = enum_values
-
-        return enums
-
-    def get_source_files(self) -> list[str]:
-        if self.server.settings.CLIENT_DEBUG:
-            return [
-                '_lona/lona.js',
-                *SOURCE_FILES,
-            ]
-
-        return [
-            '_lona/lona.js',
-        ]
-
-    def compile(self) -> None:
-        logger.debug('pre compiling client')
-
-        if self.server.settings.CLIENT_DEBUG:
-            logger.debug('running in debug mode')
-
-        for source_file in self.get_source_files():
-            logger.debug('pre compiling %s', source_file)
-
-            try:
-                template = self.jinja2_env.get_template(source_file)
-
-                template_context = {
-                    'DEBUG': self.server.settings.CLIENT_DEBUG,
-                    'source_files': SOURCE_FILES,
-                    'protocol': dumps(self.get_enums()),
-                    'settings': dumps(self.get_settings()),
-                }
-
-                file_content = template.render(
-                    **template_context,
-                )
-
-                path = os.path.join(
-                    self.tmp_dir.name,
-                    source_file,
-                )
-
-                with open(path, 'w+') as f:
-                    f.write(file_content)
-
-            except Exception:  # pragma: no cover
-                logger.exception(
-                    'exception raised while pre compiling %s',
-                    source_file,
-                )
-
-    def resolve_path(self, path: str) -> str:
-        full_path = os.path.join(self.tmp_dir.name, path)
-
-        if not os.path.exists(full_path):
-            return ''
-
-        if self.server.settings.CLIENT_DEBUG:
-            self.compile()
-
-        return full_path
-
-    def __repr__(self):
-        return f'<ClientPreCompiler({self.path})>'  # pragma: no cover

lona/server.py
@@ -25,7 +25,6 @@ from jinja2 import Template
 
 from lona.view_runtime_controller import ViewRuntimeController
 from lona.middleware_controller import MiddlewareController
-from lona.client_pre_compiler import ClientPreCompiler
 from lona.static_file_loader import StaticFileLoader
 from lona.response_parser import ResponseParser
 from lona.templating import TemplatingEngine
@@ -174,7 +173,6 @@ class LonaServer:
         self._view_runtime_controller = ViewRuntimeController(self)
 
         # setup static files
-        self._client_pre_compiler: ClientPreCompiler = ClientPreCompiler(self)
 
         # the static file loader has to be started last because it does
         # node class discovery which has to happen after all views are imported

commit 3e0dc3a2a9041989db2ef3805b392600e59f03f7

fix(collect-static): remove usage of old client pre compiler API
This patch fixes a regression, introduced in
7d8b5513b72d ("client: remove client pre compiler").

Before 7d8b5513b72d, the collect-static mechanism used the client pre-compiler
to get a list of all client JavaScript files to copy to the output. This API
is no longer available, because the client pre-compiler was removed.

This patch creates a new method in the static file loader, which returns all
found static files, and changes collect-static to use it.

Signed-off-by: Florian Scherf <mail@florianscherf.de>

lona/command_line/collect_static.py
@@ -1,4 +1,3 @@
-import contextlib
 import shutil
 import os
 
@@ -24,15 +23,6 @@ def collect_static(args):
 
         else:
             os.unlink(path)
-
-    def _mkdir(path):
-        _print(f'mkdir -p {path}')
-
-        if args.dry_run:
-            return
-
-        with contextlib.suppress(FileExistsError):
-            os.makedirs(path)
 
     def _cp(source, destination):
         source_is_dir = os.path.isdir(source)
@@ -76,34 +66,8 @@ def collect_static(args):
             path = os.path.join(args.destination, name)
             _rm(path)
 
-    # copy node static files
-    for static_file in server._static_file_loader.static_files[::-1]:
-        if not static_file.enabled:
-            continue
+    # copy files
+    for path, url in server._static_file_loader.get_paths():
+        destination = os.path.join(args.destination, url)
 
-        destination = os.path.join(
-            args.destination,
-            static_file.url,
-        )
-
-        _cp(static_file.path, destination)
-
-    # copy static files from static directories
-    for static_dir in server._static_file_loader.static_dirs[::-1]:
-        for name in os.listdir(static_dir):
-            source = os.path.join(static_dir, name)
-            destination = os.path.join(args.destination, name)
-
-            _cp(source, destination)
-
-    # copy client
-    for source_file in server._client_pre_compiler.get_source_files():
-        source = server._client_pre_compiler.resolve_path(source_file)
-
-        destination = os.path.join(
-            args.destination,
-            source_file,
-        )
-
-        _mkdir(os.path.dirname(destination))
-        _cp(source, destination)
+        _cp(path, destination)

lona/static_file_loader.py
@@ -241,3 +241,19 @@ class StaticFileLoader:
         logger.debug("'%s' was not found", path)
 
         return None
+
+    def get_paths(self) -> Iterator[tuple[str, str]]:
+
+        # copy node static files
+        for static_file in self.static_files[::-1]:
+            if not static_file.enabled:
+                continue
+
+            yield static_file.path, static_file.url
+
+        # static directories
+        for static_dir in self.static_dirs[::-1]:
+            for name in os.listdir(static_dir):
+                path = os.path.join(static_dir, name)
+
+                yield path, name