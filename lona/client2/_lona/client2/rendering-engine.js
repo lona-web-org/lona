ed9796a04d3b50ee061aaedd8e2021ca925e81b7
Florian Scherf <mail@florianscherf.de>
2022-11-24 19:24:57 +0100

    client2: rendering: refactor render_node
    This method was deeply nested before.
    
    This patch refactors the method, without any functional changes.
    
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- lona/client2/_lona/client2/rendering-engine.js
+++ lona/client2/_lona/client2/rendering-engine.js
@@ -158,106 +158,106 @@ export class LonaRenderingEngine {
     // html rendering ---------------------------------------------------------
     _render_node(node_spec) {
         var property_names = ['value', 'checked', 'selected'];
-
         var node_type = node_spec[0];
 
-        // Node
-        if(node_type == Lona.protocol.NODE_TYPE.NODE) {
-            var node_id = node_spec[1];
-            var node_tag_name = node_spec[2];
-            var node_id_list = node_spec[3];
-            var node_class_list = node_spec[4];
-            var node_style = node_spec[5];
-            var node_attributes = node_spec[6];
-            var node_child_nodes = node_spec[7];
-            var widget_class_name = node_spec[8];
-            var widget_data = node_spec[9];
-
-            var node = document.createElement(node_tag_name);
-
-            // lona node id
-            node.setAttribute('data-lona-node-id', node_id);
-
-            // id list
-            if(node_id_list.length > 0) {
-                this._add_id(node, node_id_list);
-            };
-
-            // class list
-            if(node_class_list.length > 0) {
-                node.classList = node_class_list.join(' ').trim();
-            };
-
-            // style
-            if(Object.keys(node_style).length > 0) {
-                Object.keys(node_style).forEach(key => {
-                    node.style[key] = node_style[key];
-                });
-            };
-
-            // attributes
-            if(Object.keys(node_attributes).length > 0) {
-                Object.keys(node_attributes).forEach(key => {
-                    var value = node_attributes[key];
-
-                    // properties
-                    if(property_names.indexOf(key) > -1) {
-                        if(key != 'value' && node_attributes[key] !== false) {
-                            value = true;
-                        };
-
-                        node[key] = value;
-
-                    // attributes
-                    } else {
-                        node.setAttribute(key, node_attributes[key]);
-
-                    };
-                });
-            };
-
-            // nodes
-            node_child_nodes.forEach(child_node_argspec => {
-                var child_node = this._render_node(child_node_argspec);
-
-                node.appendChild(child_node);
-            });
-
-            this._nodes[node_id] = node;
-
-            // widget
-            if(widget_class_name != '') {
-                if(!(widget_class_name in Lona.widget_classes)) {
-                    throw(`RuntimeError: unknown widget name '${widget_class_name}'`);
-                }
-
-                var widget_class = Lona.widget_classes[widget_class_name];
-
-                var window_shim = new LonaWindowShim(
-                    this.lona_context,
-                    this.lona_window,
-                    node_id,
-                );
-
-                var widget = new widget_class(window_shim);
-
-                this._widgets[node_id] = widget;
-                this._widget_data[node_id] = widget_data;
-                this._widgets_to_setup.splice(0, 0, node_id);
-            }
-
-            // patch input events
-            this.lona_window._input_event_handler.patch_input_events(node);
-
         // TextNode
-        } else if(node_type == Lona.protocol.NODE_TYPE.TEXT_NODE) {
+        if(node_type == Lona.protocol.NODE_TYPE.TEXT_NODE) {
             var node_id = node_spec[1];
             var node_content = node_spec[2];
 
             var node = document.createTextNode(node_content);
 
             this._nodes[node_id] = node;
+
+            return node;
         };
+
+        // Node
+        var node_id = node_spec[1];
+        var node_tag_name = node_spec[2];
+        var node_id_list = node_spec[3];
+        var node_class_list = node_spec[4];
+        var node_style = node_spec[5];
+        var node_attributes = node_spec[6];
+        var node_child_nodes = node_spec[7];
+        var widget_class_name = node_spec[8];
+        var widget_data = node_spec[9];
+
+        var node = document.createElement(node_tag_name);
+
+        // lona node id
+        node.setAttribute('data-lona-node-id', node_id);
+
+        // id list
+        if(node_id_list.length > 0) {
+            this._add_id(node, node_id_list);
+        };
+
+        // class list
+        if(node_class_list.length > 0) {
+            node.classList = node_class_list.join(' ').trim();
+        };
+
+        // style
+        if(Object.keys(node_style).length > 0) {
+            Object.keys(node_style).forEach(key => {
+                node.style[key] = node_style[key];
+            });
+        };
+
+        // attributes
+        if(Object.keys(node_attributes).length > 0) {
+            Object.keys(node_attributes).forEach(key => {
+                var value = node_attributes[key];
+
+                // properties
+                if(property_names.indexOf(key) > -1) {
+                    if(key != 'value' && node_attributes[key] !== false) {
+                        value = true;
+                    };
+
+                    node[key] = value;
+
+                // attributes
+                } else {
+                    node.setAttribute(key, node_attributes[key]);
+
+                };
+            });
+        };
+
+        // nodes
+        node_child_nodes.forEach(child_node_argspec => {
+            var child_node = this._render_node(child_node_argspec);
+
+            node.appendChild(child_node);
+        });
+
+        this._nodes[node_id] = node;
+
+        // widget
+        if(widget_class_name != '') {
+            if(!(widget_class_name in Lona.widget_classes)) {
+                throw(`RuntimeError: unknown widget name '${widget_class_name}'`);
+            }
+
+            var widget_class = Lona.widget_classes[widget_class_name];
+
+            var window_shim = new LonaWindowShim(
+                this.lona_context,
+                this.lona_window,
+                node_id,
+            );
+
+            var widget = new widget_class(window_shim);
+
+            this._widgets[node_id] = widget;
+            this._widget_data[node_id] = widget_data;
+            this._widgets_to_setup.splice(0, 0, node_id);
+        }
+
+        // patch input events
+        this.lona_window._input_event_handler.patch_input_events(node);
 
         return node;
     };
