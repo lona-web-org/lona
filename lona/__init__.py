4fdd7c032fe6e60a260a20c7dc4edf0081d62734
Florian Scherf <mail@florianscherf.de>
2022-12-19 14:23:15 +0100

    packaging: remove old import error mitigation code
    All Lona versions prior to 1.10.5.1 imported `lona.VERSION` while packaging
    and installation. This import got removed due import problems on multiple
    occasions.
    
    This patch removes the last remains of previous mitigations of these problems.
    
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- lona/__init__.py
+++ lona/__init__.py
@@ -1,18 +1,12 @@
 # TODO: remove LonaView and LonaApp in 2.0
 
-try:
-    from .exceptions import *  # NOQA: F403
-    from .routing import MATCH_ALL, Route
-    from .errors import *  # NOQA: F403
-    from .view import View as LonaView
-    from .app import App as LonaApp
-    from .view import View
-    from .app import App
-
-except ImportError as e:  # pragma: no cover
-    # this can happen while installing the package and can be ignored
-    if e.name != 'typing_extensions':
-        raise
+from .exceptions import *  # NOQA: F403
+from .routing import MATCH_ALL, Route
+from .errors import *  # NOQA: F403
+from .view import View as LonaView
+from .app import App as LonaApp
+from .view import View
+from .app import App
 
 VERSION = (1, 10, 5, 1)
 VERSION_STRING = '.'.join(str(i) for i in VERSION)
