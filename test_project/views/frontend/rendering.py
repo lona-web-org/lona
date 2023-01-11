3d1c86e3e0a2c36a70537f2afc3f14465a9b0f42
Florian Scherf <mail@florianscherf.de>
2023-01-11 14:29:21 +0100

    tests: rendering: reduce locking boilerplate code
    Previously, every step had to acquire its lock explicitly, before running its
    code.
    
    This patch moves the locking code into the `run_next_step` method, which calls
    the individual steps.
    
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- test_project/views/frontend/rendering.py
+++ test_project/views/frontend/rendering.py
@@ -263,15 +263,16 @@ class RenderingTestView(View):
         return steps
 
     def run_next_step(self):
-        step = self.steps[self.current_step_index]
+        with self.html.lock:
+            step = self.steps[self.current_step_index]
 
-        step()
-        self.html_console.update()
+            step()
+            self.html_console.update()
 
-        self.current_step_index += 1
+            self.current_step_index += 1
 
-        if self.current_step_index >= len(self.steps):
-            self.current_step_index = 0
+            if self.current_step_index >= len(self.steps):
+                self.current_step_index = 0
 
     def set_step_label(self, step_number, label_text):
         with self.html.lock:
@@ -291,292 +292,254 @@ class RenderingTestView(View):
     # steps ###################################################################
     # node tests
     def step_01(self):
-        with self.html.lock:
-            self.set_step_label(1, 'Clear Nodes')
+        self.set_step_label(1, 'Clear Nodes')
 
-            self.rendering_root.clear()
+        self.rendering_root.clear()
 
     def step_02(self):
-        with self.html.lock:
-            self.set_step_label(2, 'Append Nodes')
+        self.set_step_label(2, 'Append Nodes')
 
-            self.rendering_root.append(Div('first div'))
-            self.rendering_root.append(Div('second div'))
+        self.rendering_root.append(Div('first div'))
+        self.rendering_root.append(Div('second div'))
 
     def step_03(self):
-        with self.html.lock:
-            self.set_step_label(3, 'Set Node')
+        self.set_step_label(3, 'Set Node')
 
-            self.rendering_root[0] = Div('set div')
+        self.rendering_root[0] = Div('set div')
 
     def step_04(self):
-        with self.html.lock:
-            self.set_step_label(4, 'Reset Nodes')
+        self.set_step_label(4, 'Reset Nodes')
 
-            self.rendering_root.nodes = [
-                Div('reset div1'),
-                Div('reset div2'),
-            ]
+        self.rendering_root.nodes = [
+            Div('reset div1'),
+            Div('reset div2'),
+        ]
 
     def step_05(self):
-        with self.html.lock:
-            self.set_step_label(5, 'Insert Node')
+        self.set_step_label(5, 'Insert Node')
 
-            self.rendering_root.nodes.insert(1, Div('inserted div'))
+        self.rendering_root.nodes.insert(1, Div('inserted div'))
 
     def step_06(self):
-        with self.html.lock:
-            self.set_step_label(6, 'Remove Node')
+        self.set_step_label(6, 'Remove Node')
 
-            self.rendering_root.nodes[1].remove()
+        self.rendering_root.nodes[1].remove()
 
     # id_list tests
     def step_07(self):
-        with self.html.lock:
-            self.set_step_label(7, 'Set id')
+        self.set_step_label(7, 'Set id')
 
-            self.rendering_root.nodes = [
-                Div(_id='foo bar'),
-            ]
+        self.rendering_root.nodes = [
+            Div(_id='foo bar'),
+        ]
 
     def step_08(self):
-        with self.html.lock:
-            self.set_step_label(8, 'Add id')
+        self.set_step_label(8, 'Add id')
 
-            self.rendering_root.nodes[0].id_list.add('baz')
+        self.rendering_root.nodes[0].id_list.add('baz')
 
     def step_09(self):
-        with self.html.lock:
-            self.set_step_label(9, 'Remove id')
+        self.set_step_label(9, 'Remove id')
 
-            self.rendering_root.nodes[0].id_list.remove('bar')
+        self.rendering_root.nodes[0].id_list.remove('bar')
 
     def step_10(self):
-        with self.html.lock:
-            self.set_step_label(10, 'Reset id')
+        self.set_step_label(10, 'Reset id')
 
-            self.rendering_root.nodes[0].id_list = ['foo1', 'bar1']
+        self.rendering_root.nodes[0].id_list = ['foo1', 'bar1']
 
     def step_11(self):
-        with self.html.lock:
-            self.set_step_label(11, 'Clear id')
+        self.set_step_label(11, 'Clear id')
 
-            self.rendering_root.nodes[0].id_list.clear()
+        self.rendering_root.nodes[0].id_list.clear()
 
     # class_list tests
     def step_12(self):
-        with self.html.lock:
-            self.set_step_label(12, 'Set class')
+        self.set_step_label(12, 'Set class')
 
-            self.rendering_root.nodes = [
-                Div(_class='foo bar'),
-            ]
+        self.rendering_root.nodes = [
+            Div(_class='foo bar'),
+        ]
 
     def step_13(self):
-        with self.html.lock:
-            self.set_step_label(13, 'Add class')
+        self.set_step_label(13, 'Add class')
 
-            self.rendering_root.nodes[0].class_list.add('baz')
+        self.rendering_root.nodes[0].class_list.add('baz')
 
     def step_14(self):
-        with self.html.lock:
-            self.set_step_label(14, 'Remove class')
+        self.set_step_label(14, 'Remove class')
 
-            self.rendering_root.nodes[0].class_list.remove('bar')
+        self.rendering_root.nodes[0].class_list.remove('bar')
 
     def step_15(self):
-        with self.html.lock:
-            self.set_step_label(15, 'Reset class')
+        self.set_step_label(15, 'Reset class')
 
-            self.rendering_root.nodes[0].class_list = ['foo1', 'bar1']
+        self.rendering_root.nodes[0].class_list = ['foo1', 'bar1']
 
     def step_16(self):
-        with self.html.lock:
-            self.set_step_label(16, 'Clear class')
+        self.set_step_label(16, 'Clear class')
 
-            self.rendering_root.nodes[0].class_list.clear()
+        self.rendering_root.nodes[0].class_list.clear()
 
     # attribute tests
     def step_17(self):
-        with self.html.lock:
-            self.set_step_label(17, 'Set attributes')
+        self.set_step_label(17, 'Set attributes')
 
-            self.rendering_root.nodes = [
-                Div(foo='foo', bar='bar'),
-            ]
+        self.rendering_root.nodes = [
+            Div(foo='foo', bar='bar'),
+        ]
 
     def step_18(self):
-        with self.html.lock:
-            self.set_step_label(18, 'Add attribute')
+        self.set_step_label(18, 'Add attribute')
 
-            self.rendering_root.nodes[0].attributes['baz'] = 'baz'
+        self.rendering_root.nodes[0].attributes['baz'] = 'baz'
 
     def step_19(self):
-        with self.html.lock:
-            self.set_step_label(19, 'Remove attribute')
+        self.set_step_label(19, 'Remove attribute')
 
-            del self.rendering_root.nodes[0].attributes['foo']
+        del self.rendering_root.nodes[0].attributes['foo']
 
     def step_20(self):
-        with self.html.lock:
-            self.set_step_label(20, 'Reset attributes')
+        self.set_step_label(20, 'Reset attributes')
 
-            self.rendering_root.nodes[0].attributes = {
-                'foo1': 'bar1',
-                'bar1': 'foo1',
-            }
+        self.rendering_root.nodes[0].attributes = {
+            'foo1': 'bar1',
+            'bar1': 'foo1',
+        }
 
     def step_21(self):
-        with self.html.lock:
-            self.set_step_label(21, 'Clear attributes')
+        self.set_step_label(21, 'Clear attributes')
 
-            self.rendering_root.nodes[0].attributes.clear()
+        self.rendering_root.nodes[0].attributes.clear()
 
     # style tests
     def step_22(self):
-        with self.html.lock:
-            self.set_step_label(22, 'Set style')
+        self.set_step_label(22, 'Set style')
 
-            self.rendering_root.nodes = [
-                Div(_style='top: 1px; right: 2px;'),
-            ]
+        self.rendering_root.nodes = [
+            Div(_style='top: 1px; right: 2px;'),
+        ]
 
     def step_23(self):
-        with self.html.lock:
-            self.set_step_label(23, 'Add style')
+        self.set_step_label(23, 'Add style')
 
-            self.rendering_root.nodes[0].style['bottom'] = '3px'
+        self.rendering_root.nodes[0].style['bottom'] = '3px'
 
     def step_24(self):
-        with self.html.lock:
-            self.set_step_label(24, 'Remove style')
+        self.set_step_label(24, 'Remove style')
 
-            del self.rendering_root.nodes[0].style['top']
+        del self.rendering_root.nodes[0].style['top']
 
     def step_25(self):
-        with self.html.lock:
-            self.set_step_label(25, 'Reset style')
+        self.set_step_label(25, 'Reset style')
 
-            self.rendering_root.nodes[0].style = {
-                'left': '4px',
-            }
+        self.rendering_root.nodes[0].style = {
+            'left': '4px',
+        }
 
     def step_26(self):
-        with self.html.lock:
-            self.set_step_label(26, 'Clear style')
+        self.set_step_label(26, 'Clear style')
 
-            self.rendering_root.nodes[0].style.clear()
+        self.rendering_root.nodes[0].style.clear()
 
     # widget data tests
     def step_27(self):
-        with self.html.lock:
-            self.set_step_label(27, 'Widget Data: list: setup')
+        self.set_step_label(27, 'Widget Data: list: setup')
 
-            self.rendering_root.nodes = [
-                WidgetDataTestComponent(
-                    initial_state={'list': []},
-                ),
-            ]
+        self.rendering_root.nodes = [
+            WidgetDataTestComponent(
+                initial_state={'list': []},
+            ),
+        ]
 
     def step_28(self):
-        with self.html.lock:
-            self.set_step_label(28, 'Widget Data: list: append')
+        self.set_step_label(28, 'Widget Data: list: append')
 
-            component = self.rendering_root.nodes[0]
+        component = self.rendering_root.nodes[0]
 
-            component.widget_data['list'].append(1)
-            component.widget_data['list'].append(2)
-            component.widget_data['list'].append(3)
-            component.update_state()
+        component.widget_data['list'].append(1)
+        component.widget_data['list'].append(2)
+        component.widget_data['list'].append(3)
+        component.update_state()
 
     def step_29(self):
-        with self.html.lock:
-            self.set_step_label(29, 'Widget Data: list: remove')
+        self.set_step_label(29, 'Widget Data: list: remove')
 
-            component = self.rendering_root.nodes[0]
+        component = self.rendering_root.nodes[0]
 
-            component.widget_data['list'].remove(2)
-            component.update_state()
+        component.widget_data['list'].remove(2)
+        component.update_state()
 
     def step_30(self):
-        with self.html.lock:
-            self.set_step_label(30, 'Widget Data: list: insert')
+        self.set_step_label(30, 'Widget Data: list: insert')
 
-            component = self.rendering_root.nodes[0]
+        component = self.rendering_root.nodes[0]
 
-            component.widget_data['list'].insert(0, 0)
-            component.update_state()
+        component.widget_data['list'].insert(0, 0)
+        component.update_state()
 
     def step_31(self):
-        with self.html.lock:
-            self.set_step_label(31, 'Widget Data: list: clear')
+        self.set_step_label(31, 'Widget Data: list: clear')
 
-            component = self.rendering_root.nodes[0]
+        component = self.rendering_root.nodes[0]
 
-            component.widget_data['list'].clear()
-            component.update_state()
+        component.widget_data['list'].clear()
+        component.update_state()
 
     def step_32(self):
-        with self.html.lock:
-            self.set_step_label(32, 'Widget Data: list: reset')
+        self.set_step_label(32, 'Widget Data: list: reset')
 
-            component = self.rendering_root.nodes[0]
+        component = self.rendering_root.nodes[0]
 
-            component.widget_data['list'] = [5, 4, 3, 2, 1]
-            component.update_state()
+        component.widget_data['list'] = [5, 4, 3, 2, 1]
+        component.update_state()
 
     def step_33(self):
-        with self.html.lock:
-            self.set_step_label(33, 'Widget Data: dict: setup')
+        self.set_step_label(33, 'Widget Data: dict: setup')
 
-            component = self.rendering_root.nodes[0]
+        component = self.rendering_root.nodes[0]
 
-            component.widget_data = {'dict': {}}
-            component.update_state()
+        component.widget_data = {'dict': {}}
+        component.update_state()
 
     def step_34(self):
-        with self.html.lock:
-            self.set_step_label(34, 'Widget Data: dict: set')
+        self.set_step_label(34, 'Widget Data: dict: set')
 
-            component = self.rendering_root.nodes[0]
+        component = self.rendering_root.nodes[0]
 
-            component.widget_data['dict'][1] = 1
-            component.widget_data['dict'][2] = 2
-            component.widget_data['dict'][3] = 3
-            component.update_state()
+        component.widget_data['dict'][1] = 1
+        component.widget_data['dict'][2] = 2
+        component.widget_data['dict'][3] = 3
+        component.update_state()
 
     def step_35(self):
-        with self.html.lock:
-            self.set_step_label(35, 'Widget Data: dict: del')
+        self.set_step_label(35, 'Widget Data: dict: del')
 
-            component = self.rendering_root.nodes[0]
+        component = self.rendering_root.nodes[0]
 
-            del component.widget_data['dict'][2]
-            component.update_state()
+        del component.widget_data['dict'][2]
+        component.update_state()
 
     def step_36(self):
-        with self.html.lock:
-            self.set_step_label(36, 'Widget Data: dict: pop')
+        self.set_step_label(36, 'Widget Data: dict: pop')
 
-            component = self.rendering_root.nodes[0]
+        component = self.rendering_root.nodes[0]
 
-            component.widget_data['dict'].pop(3)
-            component.update_state()
+        component.widget_data['dict'].pop(3)
+        component.update_state()
 
     def step_37(self):
-        with self.html.lock:
-            self.set_step_label(37, 'Widget Data: dict: clear')
+        self.set_step_label(37, 'Widget Data: dict: clear')
 
-            component = self.rendering_root.nodes[0]
+        component = self.rendering_root.nodes[0]
 
-            component.widget_data['dict'].clear()
-            component.update_state()
+        component.widget_data['dict'].clear()
+        component.update_state()
 
     def step_38(self):
-        with self.html.lock:
-            self.set_step_label(38, 'Widget Data: dict: reset')
+        self.set_step_label(38, 'Widget Data: dict: reset')
 
-            component = self.rendering_root.nodes[0]
+        component = self.rendering_root.nodes[0]
 
-            component.widget_data['dict'] = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4}
-            component.update_state()
+        component.widget_data['dict'] = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4}
+        component.update_state()
