lona-web-org/lona

commit ef19a2e45b0802928b230f7d13ba85afbd0655f4

setup response classes
Previously, Lona views used special dictionaries as responses, instead of
proper response classes like any other Python web framework.

This is a design that was part of Lona since the very beginning. In the early
days of this framework, views were simple functions, that needed almost no
imports. The idea was to use a Python standard data structure, so no classes
have to be imported, and no response class names have to be remembered.

This was a horrible idea, and lead to horrible code, namely the
`ResponseParser` code. Because the dictionaries could contain any key, they
had to be parsed.

This patch adds classes for any type of response, Lona supports, and a
drop-in-replacement for the response parser code, that converts dictionaries
into responses, to be backwards compatible, until Lona 2.

Signed-off-by: Florian Scherf <mail@florianscherf.de>

lona/view_runtime_controller.py
@@ -6,6 +6,7 @@ import logging
 from yarl import URL
 
 from lona.view_runtime import VIEW_RUNTIME_STATE, ViewRuntime
+from lona.responses import parse_view_return_value, Response
 from lona.protocol import encode_http_redirect, METHOD
 from lona.events.view_event import ViewEvent
 from lona.exceptions import ServerStop
@@ -257,17 +258,17 @@ class ViewRuntimeController:
             )
 
         # run middlewares
-        response_dict = view_runtime.run_middlewares(
+        response = view_runtime.run_middlewares(
             connection=connection,
             window_id=window_id,
             url=url,
         )
 
         # request got handled by a middleware
-        if response_dict:
+        if response:
             views_logger.debug('connection was refused by middleware')
 
-            return response_dict
+            return response
 
         self._view_runtimes[view_runtime.view_runtime_id] = view_runtime
 
@@ -287,14 +288,14 @@ class ViewRuntimeController:
             )
 
         else:
-            response_dict = view_runtime.start()
+            response = view_runtime.start()
 
             # Non interactive runtimes don't have to be held in memory, after
             # handle_request() finished, because they can't be daemonized or
             # receive input events.
             self.remove_view_runtime(view_runtime)
 
-            return response_dict
+            return response
 
         views_logger.debug('message handled')
 
@@ -357,7 +358,10 @@ class ViewRuntimeController:
                 view_event,
             )
 
-            return_value = view_runtime.view.on_view_event(view_event)
+            return_value = parse_view_return_value(
+                return_value=view_runtime.view.on_view_event(view_event),
+                interactive=view_runtime.interactive,
+            )
 
         except Exception:
             view_events_logger.exception(
@@ -367,20 +371,14 @@ class ViewRuntimeController:
 
             return
 
-        if not isinstance(return_value, (dict, type(None))):
+        if not isinstance(return_value, (Response, type(None))):
             exception = ValueError(f'{repr(view_runtime.view.on_view_event)} returned an unexpected type ({repr(return_value)})')
 
             view_runtime.issue_500_error(exception)
 
-        if isinstance(return_value, dict):
-            response_parser = self.server._response_parser
-
+        if isinstance(return_value, Response):
             try:
-                response_dict = response_parser.parse_event_response_dict(
-                    return_value,
-                )
-
-                view_runtime.handle_raw_response_dict(response_dict)
+                view_runtime.handle_response(response=return_value)
 
             except Exception as exception:
                 view_runtime.issue_500_error(exception)

lona/__init__.py
@@ -1,6 +1,7 @@
 # TODO: remove LonaView and LonaApp in 2.0
 
 from .exceptions import *  # NOQA: F403
+from .responses import *  # NOQA: F403
 from .routing import MATCH_ALL, Route
 from .errors import *  # NOQA: F403
 from .view import View as LonaView

lona/response_parser.py
@@ -1,141 +1,0 @@
-import logging
-
-from lona.html.abstract_node import AbstractNode
-from lona._json import dumps
-
-logger = logging.getLogger('lona.response_parser')
-
-
-class ResponseParser:
-    def __init__(self, server):
-        self.server = server
-
-    def parse_event_response_dict(
-            self,
-            raw_response_dict: dict,
-    ) -> dict:
-
-        """
-        Parse response dicts coming from event handlers.
-
-        response dicts can only be redirects or http redirects.
-
-        examples:
-            {'redirect': '/foo'}
-            {'http_redirect': '/bar'}
-        """
-
-        supported_keys = ('redirect', 'http_redirect')
-
-        if (len(raw_response_dict.keys()) > 1 or
-                list(raw_response_dict.keys())[0] not in supported_keys):
-
-            raise ValueError(f'response dict has unexpected keys ({repr(list(raw_response_dict.keys()))})')
-
-        redirect_type, redirect_target = list(raw_response_dict.items())[0]
-
-        if not isinstance(redirect_target, str):
-            raise ValueError('redirect target has to be string')
-
-        return raw_response_dict
-
-    def render_response_dict(self, raw_response_dict, view_name):
-        response_dict = {
-            'status': 200,
-            'content_type': 'text/html',
-            'text': None,
-            'body': None,
-            'headers': None,
-            'file': '',
-            'redirect': '',
-            'http_redirect': '',
-        }
-
-        key_words = {
-            'text',
-            'body',
-            'redirect',
-            'http_redirect',
-            'template',
-            'template_string',
-            'file',
-            'json',
-        }
-
-        # Node response
-        if isinstance(raw_response_dict, AbstractNode):
-            response_dict['text'] = str(raw_response_dict)
-
-            return response_dict
-
-        # string response
-        if isinstance(raw_response_dict, str):
-            logger.debug("'%s' is a string based view", view_name)
-
-            response_dict['text'] = raw_response_dict
-
-            return response_dict
-
-        # response dict
-        elif isinstance(raw_response_dict, dict):
-
-            # check keys
-            if len(set(raw_response_dict) & key_words) != 1:
-                raise RuntimeError(
-                    'response dicts have to contain exactly one keyword of: {}'.format(  # NOQA: LN001,FS002
-                        ', '.join(key_words),
-                    ),
-                )
-
-            # find keys
-            for key in response_dict.keys():
-                if key in raw_response_dict:
-                    value = raw_response_dict[key]
-                    response_dict[key] = value
-
-                    logger.debug(
-                        "'%s' sets '%s' to %r", view_name, key, value)
-
-        # redirects
-        if 'redirect' in raw_response_dict:
-            response_dict['redirect'] = raw_response_dict['redirect']
-
-        # http redirect
-        elif 'http_redirect' in raw_response_dict:
-            response_dict['http_redirect'] = raw_response_dict['http_redirect']
-
-        # template response
-        elif ('template' in raw_response_dict or
-              'template_string' in raw_response_dict):
-
-            logger.debug("'%s' is a template view", view_name)
-
-            template_context = raw_response_dict
-
-            if 'context' in template_context:
-                template_context = template_context['context']
-
-            # template file
-            if 'template' in raw_response_dict:
-                response_dict['text'] = \
-                    self.server.render_template(
-                        raw_response_dict['template'],
-                        template_context,
-                    )
-
-            # template string
-            else:
-                response_dict['text'] = \
-                    self.server.render_string(
-                        raw_response_dict['template_string'],
-                        template_context,
-                    )
-
-        # json response
-        elif 'json' in raw_response_dict:
-            logger.debug("'%s' is a json view", view_name)
-
-            response_dict['text'] = dumps(raw_response_dict['json'])
-            response_dict['content_type'] = 'application/json'
-
-        return response_dict

lona/responses.py
@@ -1,0 +1,317 @@
+from __future__ import annotations
+
+from typing import Union, Dict, Any
+from dataclasses import dataclass
+
+from lona.html.abstract_node import AbstractNode
+from lona.events.input_event import InputEvent
+
+
+@dataclass
+class AbstractResponse:
+    @property
+    def interactive(self) -> bool:
+        raise NotImplementedError()
+
+
+@dataclass
+class Response(AbstractResponse):
+    body: Any = None
+    status: int | None = None
+    content_type: str | None = None
+    headers: Dict[str, str] | None = None
+    text: str | None = None
+    charset: str | None = None
+
+    def __post_init__(self):
+        if self.body and self.text:
+            raise ValueError('body and text are not allowed together')
+
+    @property
+    def interactive(self) -> bool:
+        if (self.body or self.status or self.content_type or
+                self.headers or self.charset):
+
+            return False
+
+        return True
+
+
+@dataclass
+class HtmlResponse(AbstractResponse):
+    html: str | AbstractNode
+    status: int | None = None
+    content_type: str | None = None
+    headers: dict | None = None
+
+    @property
+    def interactive(self) -> bool:
+        if self.status or self.content_type or self.headers:
+            return False
+
+        return True
+
+
+@dataclass
+class TemplateResponse(AbstractResponse):
+    name: str
+    context: dict | None
+    status: int | None = None
+    content_type: str | None = None
+    headers: dict | None = None
+
+    @property
+    def interactive(self) -> bool:
+        if self.status or self.content_type or self.headers:
+            return False
+
+        return True
+
+
+@dataclass
+class TemplateStringResponse(AbstractResponse):
+    string: str
+    context: dict | None = None
+    status: int | None = None
+    content_type: str | None = None
+    headers: dict | None = None
+
+    @property
+    def interactive(self) -> bool:
+        if self.status or self.content_type or self.headers:
+            return False
+
+        return True
+
+
+@dataclass
+class JsonResponse(AbstractResponse):
+    data: Any
+    status: int | None = None
+    content_type: str | None = None
+    headers: dict | None = None
+
+    @property
+    def interactive(self) -> bool:
+        return False
+
+
+@dataclass
+class FileResponse(AbstractResponse):
+    path: str
+    status: int | None = None
+    headers: dict | None = None
+
+    @property
+    def interactive(self) -> bool:
+        return False
+
+
+@dataclass
+class RedirectResponse(AbstractResponse):
+    url: str
+    headers: dict | None = None
+
+    @property
+    def interactive(self) -> bool:
+        if self.headers:
+            return False
+
+        return True
+
+
+@dataclass
+class HttpRedirectResponse(AbstractResponse):
+    url: str
+    headers: dict | None = None
+
+    @property
+    def interactive(self) -> bool:
+        if self.headers:
+            return False
+
+        return True
+
+
+# response parsing ############################################################
+# view return values
+def _parse_legacy_view_return_value(
+        return_value: dict,
+) -> AbstractResponse:
+
+    key_words = {
+        'text',
+        'body',
+        'redirect',
+        'http_redirect',
+        'template',
+        'template_string',
+        'file',
+        'json',
+    }
+
+    # check keys
+    if len(set(return_value) & key_words) != 1:
+        raise RuntimeError(
+            'response dicts have to contain exactly one keyword of: {}'.format(  # NOQA: FS002
+                ', '.join(key_words),
+            ),
+        )
+
+    # redirects
+    if 'redirect' in return_value:
+        return RedirectResponse(
+            url=str(return_value['redirect']),
+            headers=return_value.get('headers', None),
+        )
+
+    # http redirect
+    if 'http_redirect' in return_value:
+        return HttpRedirectResponse(
+            url=str(return_value['http_redirect']),
+            headers=return_value.get('headers', None),
+        )
+
+    # json
+    if 'json' in return_value:
+        return JsonResponse(
+            data=return_value['json'],
+            status=return_value.get('status', None),
+            content_type=return_value.get('content_type', None),
+            headers=return_value.get('headers', None),
+        )
+
+    # file
+    if 'file' in return_value:
+        return FileResponse(
+            path=return_value['file'],
+            status=return_value.get('status', None),
+            headers=return_value.get('headers', None),
+        )
+
+    context: dict = return_value
+
+    if 'context' in context:
+        context = context['context']
+
+    # template
+    if 'template' in return_value:
+        return TemplateResponse(
+            name=str(return_value['template']),
+            context=context,
+            status=return_value.get('status', None),
+            content_type=return_value.get('content_type', None),
+            headers=return_value.get('headers', None),
+        )
+
+    # template string
+    if 'template_string' in return_value:
+        return TemplateStringResponse(
+            string=str(return_value['template_string']),
+            context=context,
+            status=return_value.get('status', None),
+            content_type=return_value.get('content_type', None),
+            headers=return_value.get('headers', None),
+        )
+
+    return Response(
+        body=return_value.get('body', None),
+        status=return_value.get('status', None),
+        content_type=return_value.get('content_type', None),
+        headers=return_value.get('headers', None),
+        text=return_value.get('text', None),
+        charset=return_value.get('charset', None),
+    )
+
+
+def parse_view_return_value(
+        return_value: AbstractResponse | AbstractNode | str | None | dict,
+        interactive: bool,
+) -> AbstractResponse | None:
+
+    if return_value is None:
+        return return_value
+
+    # legacy view return values
+    # TODO: remove in 2.0
+    if isinstance(return_value, dict):
+        return_value = _parse_legacy_view_return_value(
+            return_value=return_value,
+        )
+
+    if isinstance(return_value, (str, AbstractNode)):
+        return_value = HtmlResponse(html=return_value)
+
+    # check for non-interactive features
+    if interactive:
+
+        # some response types can not be used in interactive views
+        if isinstance(return_value, (JsonResponse, FileResponse)):
+            raise RuntimeError(
+                'JSON, binary and file responses and headers are only available in non-interactive mode',
+            )
+
+        # some response features can not be used in interactive views
+        if (not isinstance(return_value, (RedirectResponse,
+                                          HttpRedirectResponse)) and
+                not return_value.interactive):
+
+            raise RuntimeError(
+                'JSON, binary and file responses and headers are only available in non-interactive mode',
+            )
+
+    if isinstance(return_value, AbstractResponse):
+        return return_value
+
+    raise RuntimeError(
+        f'expected string, Lona Node or Lona response object. Got {type(return_value)}',
+    )
+
+
+# input event handler return values
+def _parse_legacy_input_event_handler_return_value(
+        return_value: Dict[str, str],
+) -> RedirectResponse | HttpRedirectResponse:
+
+    # TODO: remove in 2.0
+    supported_keys = ('redirect', 'http_redirect')
+
+    if (len(return_value.keys()) > 1 or
+            list(return_value.keys())[0] not in supported_keys):
+
+        raise ValueError(f'response dict has unexpected keys ({repr(list(return_value.keys()))})')
+
+    redirect_type, redirect_target = list(return_value.items())[0]
+
+    if not isinstance(redirect_target, str):
+        raise ValueError('redirect target has to be string')
+
+    if redirect_type == 'http_redirect':
+        return HttpRedirectResponse(url=redirect_target)
+
+    return RedirectResponse(url=redirect_target)
+
+
+def parse_input_event_handler_return_value(
+        return_value: Union[InputEvent, RedirectResponse, HttpRedirectResponse,
+                            None, Dict[str, str]],
+) -> InputEvent | RedirectResponse | HttpRedirectResponse | None:
+
+    if return_value is None:
+        return return_value
+
+    # legacy input event handler return values
+    # TODO: remove in 2.0
+    if isinstance(return_value, dict):
+        return_value = _parse_legacy_input_event_handler_return_value(
+            return_value=return_value,
+        )
+
+    if isinstance(return_value, (InputEvent, RedirectResponse,
+                                 HttpRedirectResponse)):
+
+        return return_value
+
+    raise RuntimeError(
+        f'expected RedirectResponse, HttpRedirectResponse, InputEvent, dict or NoneType. Got {type(return_value)}',
+    )

lona/server.py
@@ -14,6 +14,7 @@ import os
 
 from aiohttp.web import (
     WebSocketResponse,
+    json_response,
     FileResponse,
     Application,
     HTTPFound,
@@ -23,11 +24,20 @@ from typing_extensions import Literal
 from aiohttp import WSMsgType
 from jinja2 import Template
 
+from lona.responses import (
+    TemplateStringResponse,
+    HttpRedirectResponse,
+    TemplateResponse,
+    RedirectResponse,
+    JsonResponse,
+    HtmlResponse,
+)
 from lona.compat import set_use_future_node_classes, set_client_version
 from lona.view_runtime_controller import ViewRuntimeController
+from lona.responses import FileResponse as LonaFileResponse
 from lona.middleware_controller import MiddlewareController
 from lona.static_file_loader import StaticFileLoader
-from lona.response_parser import ResponseParser
+from lona.responses import Response as LonaResponse
 from lona.templating import TemplatingEngine
 from lona.imports import acquire as _acquire
 from lona.worker_pool import WorkerPool
@@ -170,11 +180,6 @@ class Server:
 
         self._view_loader = ViewLoader(self)
 
-        # setup response parser
-        server_logger.debug('setup response parser')
-
-        self._response_parser = ResponseParser(self)
-
         # setup views
         server_logger.debug('setup view runtime controller')
 
@@ -289,31 +294,95 @@ class Server:
             self._websocket_connections.remove(connection)
 
     # view helper #############################################################
-    def _render_response(self, response_dict):
-        if response_dict['file']:
-            return FileResponse(response_dict['file'])
+    def _render_response(self, lona_response):
+        status = 200
+        content_type = 'text/html'
 
-        if response_dict['redirect']:
-            raise HTTPFound(response_dict['redirect'])
-
-        if response_dict['http_redirect']:
-            raise HTTPFound(response_dict['http_redirect'])
-
-        default_headers = {
+        headers = {
             'Cache-Control': 'no-cache, no-store, must-revalidate',
         }
 
-        headers = response_dict['headers'] or default_headers
+        # empty response
+        if lona_response is None:
+            return Response(
+                status=status,
+                content_type=content_type,
+                headers=headers,
+                text='',
+            )
 
-        response = Response(
-            status=response_dict['status'],
-            content_type=response_dict['content_type'],
-            text=response_dict['text'],
-            body=response_dict['body'],
-            headers=headers,
+        # html responses
+        if isinstance(lona_response, HtmlResponse):
+            return Response(
+                text=str(lona_response.html),
+                status=lona_response.status or status,
+                content_type=lona_response.content_type or content_type,
+                headers=lona_response.headers or headers,
+            )
+
+        # template response
+        if isinstance(lona_response, TemplateResponse):
+            text = self.render_template(
+                template_name=lona_response.name,
+                template_context=lona_response.context,
+            )
+
+            return Response(
+                text=text,
+                status=lona_response.status or status,
+                content_type=lona_response.content_type or content_type,
+                headers=lona_response.headers or headers,
+            )
+
+        # template string response
+        if isinstance(lona_response, TemplateStringResponse):
+            text = self.render_string(
+                template_string=lona_response.string,
+                template_context=lona_response.context,
+            )
+
+            return Response(
+                text=text,
+                status=lona_response.status or status,
+                content_type=lona_response.content_type or content_type,
+                headers=lona_response.headers or headers,
+            )
+
+        # redirect responses
+        if isinstance(lona_response, (RedirectResponse, HttpRedirectResponse)):
+            raise HTTPFound(
+                location=lona_response.url,
+                headers=lona_response.headers or headers,
+            )
+
+        # json responses
+        if isinstance(lona_response, JsonResponse):
+            content_type = 'application/json'
+
+            return json_response(
+                data=lona_response.data,
+                status=lona_response.status or status,
+                content_type=lona_response.content_type or content_type,
+                headers=lona_response.headers or headers,
+            )
+
+        # file response
+        if isinstance(lona_response, LonaFileResponse):
+            return FileResponse(
+                path=lona_response.path,
+                status=lona_response.status or status,
+                headers=lona_response.headers or headers,
+            )
+
+        # fallback
+        return Response(
+            status=lona_response.status or status,
+            content_type=lona_response.content_type or content_type,
+            headers=lona_response.headers or headers,
+            charset=lona_response.charset,
+            body=lona_response.body,
+            text=lona_response.text,
         )
-
-        return response
 
     # handle http requests ####################################################
     async def _handle_static_file_request(self, request):
@@ -465,7 +534,7 @@ class Server:
                 response = await response
 
             # render and return response
-            if isinstance(response, dict):
+            if isinstance(response, LonaResponse):
                 return await self.run_function_async(
                     self._render_response,
                     response,
@@ -499,7 +568,7 @@ class Server:
         # connection got closed by middleware
         if handled:
             if data is not None:
-                if isinstance(data, dict):
+                if isinstance(data, LonaResponse):
                     return self._render_response(data)
 
                 return data
@@ -508,7 +577,7 @@ class Server:
 
         post_data = await http_request.post()
 
-        response_dict = await self.run_function_async(
+        response = await self.run_function_async(
             self._view_runtime_controller.handle_view_message,
             connection=connection,
             window_id=None,
@@ -518,7 +587,7 @@ class Server:
             executor_name='runtime_worker',
         )
 
-        return self._render_response(response_dict)
+        return self._render_response(response)
 
     # public api ##############################################################
     @overload

commit 078dec7b67335b432f861d057e6aa370232ee77c

views: fix redirects from view event handlers
This patch fixes a regression, introduced in

    ef19a2e45b08 ("setup response classes")

In ef19a2e45b08 `lona.responses.AbstractResponse` was introduced, as a new
data-structure for view responses. Since view event handlers can return
redirects, and HTTP redirects, which are now represented as sub classes of
`lona.response.AbstractResponse`, the view runtime has to check for these types
instead of dicts, like before ef19a2e45b08.

This was not covered by the original patch, and the regression was not catched
by the test, because the test did not enough checks, if a redirect from a
view event handler really worked, without any crashes.

This patch fixes the handling of `lona.responses.AbstractResponse` return
values of view event handlers, and updates the test, to check if a redirect,
actually worked.

Signed-off-by: Florian Scherf <mail@florianscherf.de>

lona/view_runtime_controller.py
@@ -5,8 +5,8 @@ import logging
 
 from yarl import URL
 
+from lona.responses import parse_view_return_value, AbstractResponse
 from lona.view_runtime import VIEW_RUNTIME_STATE, ViewRuntime
-from lona.responses import parse_view_return_value, Response
 from lona.protocol import encode_http_redirect, METHOD
 from lona.events.view_event import ViewEvent
 from lona.exceptions import ServerStop
@@ -371,12 +371,12 @@ class ViewRuntimeController:
 
             return
 
-        if not isinstance(return_value, (Response, type(None))):
+        if not isinstance(return_value, (AbstractResponse, type(None))):
             exception = ValueError(f'{repr(view_runtime.view.on_view_event)} returned an unexpected type ({repr(return_value)})')
 
             view_runtime.issue_500_error(exception)
 
-        if isinstance(return_value, Response):
+        if isinstance(return_value, AbstractResponse):
             try:
                 view_runtime.handle_response(response=return_value)
 

tests/test_redirects_from_event_handlers.py
@@ -107,7 +107,9 @@ async def test_redirects_from_event_handlers(lona_app_context):
         )
 
         await page.wait_for_url('/redirect-from-on-view-event/')
+        await page.wait_for_selector('#lona:has-text("REDIRECT FROM ON VIEW EVENT")')
 
         context.server.fire_view_event('foo')
 
         await page.wait_for_url('/')
+        await page.wait_for_selector('#lona:has-text("SUCCESS")')