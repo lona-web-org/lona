lona-web-org/lona

commit c3254581b5b24d7cd718cd1b832306f4b59ac419

views: add daemonizing support for short running views
Previously daemonizing required views their `handle_request()` method to run as
long as they wanted to be daemonized.

Initially daemonizing was planned as a feature for views that run long running
operations and push a progress in the process. Daemonizing short running views
only worked when a busy loop or a long sleep or was inserted, to artificially
prevent `handle_request()` from finishing.

This patch changes the view runtime checks so that daemonized views can be
finished without getting removed from the server.

Previously a view got daemonized by calling `LonaView.daemonize()` and
"undaemonized" and removed from the server by simply returning from
`handle_request()`. This patch adds a new boolean property called
`LonaView.is_daemon` which enables or disables if a view should be a daemon or
get removed from the server.

Because this potentially changes the flow of user application code, the new
behavior is only active when `LonaView.STOP_DAEMON_WHEN_VIEW_FINISHES` is set
to `False`.

Signed-off-by: Florian Scherf <mail@florianscherf.de>

lona/view_runtime_controller.py
@@ -37,12 +37,28 @@ class ViewRuntimeController:
 
     def get_running_view_runtime(self, user, route, match_info):
         for view_runtime in self.iter_view_runtimes():
-            if (view_runtime.start_connection.user == user and
-                    view_runtime.is_daemon and
-                    view_runtime.route == route and
-                    view_runtime.match_info == match_info):
 
-                return view_runtime
+            # basic checks: only the original user can reconnect, and only
+            # when using the same route and the same match info
+            if (view_runtime.start_connection.user != user or
+                    not view_runtime.is_daemon or
+                    view_runtime.route != route or
+                    view_runtime.match_info != match_info):
+
+                continue
+
+            # TODO: remove in 2.0
+            # compatibility for older Lona application code
+            stop_daemon_when_view_finishes = getattr(
+                view_runtime.view,
+                'STOP_DAEMON_WHEN_VIEW_FINISHES',
+                True,
+            )
+
+            if stop_daemon_when_view_finishes and view_runtime.is_stopped:
+                continue
+
+            return view_runtime
 
     def remove_view_runtime(self, view_runtime):
         with contextlib.suppress(KeyError):
@@ -192,7 +208,7 @@ class ViewRuntimeController:
                 match_info=match_info,
             )
 
-            if running_view_runtime and not running_view_runtime.is_stopped:
+            if running_view_runtime:
                 views_logger.debug('reconnecting to %r', running_view_runtime)
 
                 running_view_runtime.add_connection(

lona/view.py
@@ -26,6 +26,7 @@ H = Union[None, AbstractNode, str]
 
 class LonaView:
     STATIC_FILES: list[StaticFile] = []
+    STOP_DAEMON_WHEN_VIEW_FINISHES = True  # TODO: remove in 2.0
 
     def __init__(
             self,
@@ -45,6 +46,17 @@ class LonaView:
     @property
     def request(self) -> Request:
         return self._request
+
+    # is_daemon
+    @property
+    def is_daemon(self) -> bool:
+        return bool(self._view_runtime.is_daemon)
+
+    @is_daemon.setter
+    def is_daemon(self, value: bool) -> None:
+        self._assert_view_is_interactive()
+
+        self._view_runtime.is_daemon = bool(value)
 
     # checks ##################################################################
     def _assert_not_main_thread(self) -> None:
@@ -318,9 +330,9 @@ class LonaView:
             self._view_runtime.state = VIEW_RUNTIME_STATE.RUNNING
 
     def daemonize(self) -> None:
-        self._assert_view_is_interactive()
+        # TODO: remove in 2.0
 
-        self._view_runtime.is_daemon = True
+        self.is_daemon = True
 
     def ping(self) -> Literal['pong']:
         self._assert_view_is_interactive()

tests/test_daemon_views.py
@@ -12,8 +12,9 @@ async def test_simple_daemon_view(lona_app_context):
         from lona.html import Button, HTML, Div
         from lona import LonaView
 
-        @app.route('/')
-        class DaemonView(LonaView):
+        # TODO: remove in 2.0
+        @app.route('/async/')
+        class AsyncDaemonView(LonaView):
             def handle_request(self, request):
                 self.daemonize()
 
@@ -29,30 +30,56 @@ async def test_simple_daemon_view(lona_app_context):
                 message.set_text('view stopped')
                 self.show(html)
 
+        @app.route('/sync/')
+        class SyncDaemonView(LonaView):
+            STOP_DAEMON_WHEN_VIEW_FINISHES = False
+
+            def handle_stop_button_click(self, input_event):
+                self.is_daemon = False
+                self.message.set_text('view stopped')
+
+            def handle_request(self, request):
+                self.is_daemon = True
+
+                self.message = Div(
+                    f'view started: {datetime.now()}',
+                    _id='message',
+                )
+
+                return HTML(
+                    self.message,
+                    Button(
+                        'Stop',
+                        _id='stop',
+                        handle_click=self.handle_stop_button_click,
+                    ),
+                )
+
     context = await lona_app_context(setup_app)
 
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        browser_context = await browser.new_context()
-        page = await browser_context.new_page()
+    for url in ('/async/', '/sync/'):
+        async with async_playwright() as p:
+            browser = await p.chromium.launch()
+            browser_context = await browser.new_context()
+            page = await browser_context.new_page()
 
-        # initial load
-        await page.goto(context.make_url('/'))
-        await page.wait_for_selector('#message')
-        message1 = await page.inner_text('#message')
+            # initial load
+            await page.goto(context.make_url(url))
+            await page.wait_for_selector('#message')
+            message1 = await page.inner_text('#message')
 
-        # second load
-        await page.goto(context.make_url('/'))
-        await page.wait_for_selector('#message')
-        message2 = await page.inner_text('#message')
+            # second load
+            await page.goto(context.make_url(url))
+            await page.wait_for_selector('#message')
+            message2 = await page.inner_text('#message')
 
-        assert message1 == message2
+            assert message1 == message2
 
-        # stop view
-        await page.click('#stop')
-        message3 = await page.inner_text('#message')
+            # stop view
+            await page.click('#stop')
+            message3 = await page.inner_text('#message')
 
-        assert message3 == 'view stopped'
+            assert message3 == 'view stopped'
 
 
 async def test_multi_tab_daemon_view(lona_app_context):

commit 94587cc8627e7070733bd058ac02941bdcbbfc7c

views: fix handling of `View.is_daemon`
This patch fixes a regression, introduced in:

	c3254581b5b2 ("views: add daemonizing support for short running views")

c3254581b5b2 changed the checks, if a view runtime should be removed from the
server, to make short running deamon-views possible.

When `View.STOP_DAEMON_WHEN_VIEW_FINISHES` was set to `False` and
`View.is_daemon` to `True`, the runtime did not get removed from the server,
when the user closed the tab, and got reconnected, to the same runtime, when
reopening the tab.

When `View.STOP_DAEMON_WHEN_VIEW_FINISHES` was set to `True`, which is the
default, and `View.is_daemon` to `True`, the view should be removed from the
server, when it finishes, and the tab got closed, but instead the runtime
remained on the server, but was not reconnected when reopening the tab.

That meant that the server created a new view runtime on every access of a
page, and did neither reuse or close it, so they built up indefinitely.

This patch fixes this issue, by adding a check to
`ViewRuntime.remove_connection`, if `STOP_DAEMON_WHEN_VIEW_FINISHES` is set,
when the last connection to the runtime gets closed. In this case the runtime
gets removed from the server.

Signed-off-by: Florian Scherf <mail@florianscherf.de>

lona/view_runtime.py
@@ -120,6 +120,14 @@ class ViewRuntime:
         self.stop_reason = None
         self.is_daemon = False
 
+        # TODO: remove in 2.0
+        # compatibility for older Lona application code
+        self.stop_daemon_when_view_finishes = getattr(
+            self.view,
+            'STOP_DAEMON_WHEN_VIEW_FINISHES',
+            self.server.settings.STOP_DAEMON_WHEN_VIEW_FINISHES,
+        )
+
         self.view_runtime_id = generate_unique_id(name_space='view_runtimes')
         self.state = VIEW_RUNTIME_STATE.NOT_STARTED
         self.thread_ident = None
@@ -474,7 +482,10 @@ class ViewRuntime:
 
             # if the last connection gets closed and the view should
             # not continue running in background, it gets stopped
-            if not self.connections and not self.is_daemon:
+            if (not self.connections and
+                    self.stop_daemon_when_view_finishes and
+                    not self.is_daemon):
+
                 self.stop()
 
     # lona messages ###########################################################

lona/view_runtime_controller.py
@@ -48,15 +48,9 @@ class ViewRuntimeController:
 
                 continue
 
-            # TODO: remove in 2.0
-            # compatibility for older Lona application code
-            stop_daemon_when_view_finishes = getattr(
-                view_runtime.view,
-                'STOP_DAEMON_WHEN_VIEW_FINISHES',
-                self.server.settings.STOP_DAEMON_WHEN_VIEW_FINISHES,
-            )
+            if (view_runtime.stop_daemon_when_view_finishes and
+                    view_runtime.is_stopped):
 
-            if stop_daemon_when_view_finishes and view_runtime.is_stopped:
                 continue
 
             return view_runtime