505891ffc456a87db0a96499df134275a3a53204
Florian Scherf <mail@florianscherf.de>
2023-03-21 21:00:00 +0100

    html: rename html.nodes to html._nodes
    Currently, the standard library for node classes is not structured in a
    particular order. To change this, this patch makes room for a more structured
    `lona.html.nodes` by renaming `lona.html.nodes` to `lona.html._nodes` first,
    to maintain compatibility while transitioning, and then moving all nodes to a
    new `lona.html.nodes` module, in later patches.
    
    The resulting structure is meant to look like in the
    "MDN HTML elements reference"
    (https://developer.mozilla.org/en-US/docs/Web/HTML/Element)
    
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- lona/html/__init__.py
+++ lona/html/__init__.py
@@ -3,9 +3,9 @@ from lona.html.data_binding.inputs import *  # NOQA: F403
 from lona.html.data_binding.select import Select, Option
 from lona.html.parsing import _setup_node_classes_cache
 from lona.events.event_types import *  # NOQA: F403
+from lona.html._nodes import *  # NOQA: F403
 from lona.html.widgets import HTML as HTML1
 from lona.html.parsing import HTML as HTML2
-from lona.html.nodes import *  # NOQA: F403
 from lona.compat import get_client_version
 from lona.html.widget import Widget
 
