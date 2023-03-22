da74cefcf99d94b6307e9ffa0b41c13d55859231
Florian Scherf <mail@florianscherf.de>
2023-03-22 21:01:32 +0100

    html: nodes: move table content nodes to `html.nodes.table_content`
    This change is done to add the same structure as the
    "MDN HTML elements reference"
    (https://developer.mozilla.org/en-US/docs/Web/HTML/Element) to
    `lona.html.nodes`.
    
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- lona/html/__init__.py
+++ lona/html/__init__.py
@@ -2,6 +2,7 @@ from lona.html.nodes.inline_text_semantics import *  # NOQA: F403
 from lona.html.nodes.image_and_multimedia import *  # NOQA: F403
 from lona.html.nodes.content_sectioning import *  # NOQA: F403
 from lona.html.data_binding.select2 import Select2, Option2
+from lona.html.nodes.table_content import *  # NOQA: F403
 from lona.html.data_binding.inputs import *  # NOQA: F403
 from lona.html.nodes.text_content import *  # NOQA: F403
 from lona.html.data_binding.select import Select, Option
---
+++ lona/html/nodes/table_content.py
@@ -0,0 +1,33 @@
+from lona.html.node import Node
+
+
+class ColGroup(Node):
+    TAG_NAME = 'colgroup'
+
+
+class Table(Node):
+    TAG_NAME = 'table'
+
+
+class TBody(Node):
+    TAG_NAME = 'tbody'
+
+
+class Td(Node):
+    TAG_NAME = 'td'
+
+
+class TFoot(Node):
+    TAG_NAME = 'tfoot'
+
+
+class Th(Node):
+    TAG_NAME = 'th'
+
+
+class THead(Node):
+    TAG_NAME = 'thead'
+
+
+class Tr(Node):
+    TAG_NAME = 'tr'
