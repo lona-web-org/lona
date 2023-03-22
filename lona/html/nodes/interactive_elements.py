c7b214adfe320ca2c5fcf60e2f64329fad80fc4d
Florian Scherf <mail@florianscherf.de>
2023-03-22 23:47:35 +0100

    html: nodes: add interactive element nodes
    Reference: https://developer.mozilla.org/en-US/docs/Web/HTML/Element
    
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

---
+++ lona/html/nodes/interactive_elements.py
@@ -0,0 +1,13 @@
+from lona.html.node import Node
+
+
+class Details(Node):
+    TAG_NAME = 'details'
+
+
+class Dialog(Node):
+    TAG_NAME = 'dialog'
+
+
+class Summary(Node):
+    TAG_NAME = 'summary'
