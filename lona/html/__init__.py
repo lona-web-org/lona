f0d196305a17ab6d7bf2c94550cf9365b6baee51
Florian Scherf <mail@florianscherf.de>
2023-03-22 21:42:39 +0100

    html: nodes: move data binding nodes to `html.nodes.forms`
    This change is done to add the same structure as the
    "MDN HTML elements reference"
    (https://developer.mozilla.org/en-US/docs/Web/HTML/Element) to
    `lona.html.nodes`.
    
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- lona/html/__init__.py
+++ lona/html/__init__.py
@@ -1,12 +1,12 @@
 from lona.html.nodes.inline_text_semantics import *  # NOQA: F403
 from lona.html.nodes.image_and_multimedia import *  # NOQA: F403
 from lona.html.nodes.content_sectioning import *  # NOQA: F403
-from lona.html.data_binding.select2 import Select2, Option2
+from lona.html.nodes.forms.select2 import Select2, Option2
 from lona.html.nodes.table_content import *  # NOQA: F403
-from lona.html.data_binding.inputs import *  # NOQA: F403
 from lona.html.nodes.text_content import *  # NOQA: F403
-from lona.html.data_binding.select import Select, Option
+from lona.html.nodes.forms.inputs import *  # NOQA: F403
 from lona.html.parsing import _setup_node_classes_cache
+from lona.html.nodes.forms.select import Select, Option
 from lona.html.nodes.scripting import *  # NOQA: F403
 from lona.events.event_types import *  # NOQA: F403
 from lona.html.nodes.forms import *  # NOQA: F403
