51612cf109e7641436df61fed20289635de624fa
Florian Scherf <mail@florianscherf.de>
2023-03-22 20:52:47 +0100

    WIP: html: nodes: sectioning
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- lona/html/__init__.py
+++ lona/html/__init__.py
@@ -6,6 +6,7 @@ from lona.html.data_binding.inputs import *  # NOQA: F403
 from lona.html.nodes.text_content import *  # NOQA: F403
 from lona.html.data_binding.select import Select, Option
 from lona.html.parsing import _setup_node_classes_cache
+from lona.html.nodes.scripting import *  # NOQA: F403
 from lona.events.event_types import *  # NOQA: F403
 from lona.html._nodes import *  # NOQA: F403
 from lona.html.widgets import HTML as HTML1
