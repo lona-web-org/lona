bdf9de30249e2fcb2303a18d438e664bd0450a54
Florian Scherf <mail@florianscherf.de>
2023-03-22 23:12:30 +0100

    html: nodes: inline text semantics: add missing nodes
    Reference: https://developer.mozilla.org/en-US/docs/Web/HTML/Element
    
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- lona/html/nodes/inline_text_semantics.py
+++ lona/html/nodes/inline_text_semantics.py
@@ -28,13 +28,81 @@ class A(Node):
         return self.attributes['href']
 
 
+class Abbr(Node):
+    TAG_NAME = 'abbr'
+
+
+class B(Node):
+    TAG_NAME = 'b'
+
+
+class Bdi(Node):
+    TAG_NAME = 'bdi'
+
+
+class Bdo(Node):
+    TAG_NAME = 'bdo'
+
+
 class Br(Node):
     TAG_NAME = 'br'
     SELF_CLOSING_TAG = True
 
 
+class Cite(Node):
+    TAG_NAME = 'cite'
+
+
+class Code(Node):
+    TAG_NAME = 'code'
+
+
+class Data(Node):
+    TAG_NAME = 'data'
+
+
+class Dfn(Node):
+    TAG_NAME = 'dfn'
+
+
+class Em(Node):
+    TAG_NAME = 'em'
+
+
 class I(Node):  # NOQA: E742
     TAG_NAME = 'i'
+
+
+class Kbd(Node):
+    TAG_NAME = 'kbd'
+
+
+class Mark(Node):
+    TAG_NAME = 'mark'
+
+
+class Q(Node):
+    TAG_NAME = 'q'
+
+
+class Rp(Node):
+    TAG_NAME = 'rp'
+
+
+class Rt(Node):
+    TAG_NAME = 'rt'
+
+
+class Ruby(Node):
+    TAG_NAME = 'ruby'
+
+
+class S(Node):
+    TAG_NAME = 's'
+
+
+class Samp(Node):
+    TAG_NAME = 'samp'
 
 
 class Small(Node):
@@ -51,3 +119,24 @@ class Strong(Node):
 
 class Sub(Node):
     TAG_NAME = 'sub'
+
+
+class Sup(Node):
+    TAG_NAME = 'sup'
+
+
+class Time(Node):
+    TAG_NAME = 'time'
+
+
+class U(Node):
+    TAG_NAME = 'u'
+
+
+class Var(Node):
+    TAG_NAME = 'var'
+
+
+class Wbr(Node):
+    TAG_NAME = 'wbr'
+    SELF_CLOSING_TAG = True
