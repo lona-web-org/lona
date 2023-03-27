20d7936fe5199e3863e1f6b3c15bf6e4feb050fe
Florian Scherf <mail@florianscherf.de>
2023-03-27 21:35:29 +0200

    html: parsing: fork `html.parser.HTMLParser.parse_starttag`
    Lona uses `html.parser.HTMLParser` to convert HTML-strings to Lona nodes. This
    implementation converts all attribute names, of HTML tags, to lower case in
    `html.parser.HTMLParser.parse_starttag`, which is fine for HTML, but becomes a
    problem when parsing SVGs.
    
    To fix this in the future, this patch forks the method from the standard
    library, without changing it.
    This patch also adds some NOQAs to make the linter happy.
    
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- lona/html/parsing.py
+++ lona/html/parsing.py
@@ -1,7 +1,12 @@
 from __future__ import annotations
 
-from html.parser import HTMLParser
+from html.parser import (  # type: ignore[attr-defined]
+    attrfind_tolerant,
+    tagfind_tolerant,
+    HTMLParser,
+)
 from typing import List, Dict
+from html import unescape
 import logging
 
 from lona.compat import get_use_future_node_classes
@@ -31,6 +36,50 @@ class NodeHTMLParser(HTMLParser):
         self.use_future_node_classes = get_use_future_node_classes()
 
         super().__init__(*args, **kwargs)
+
+    # this method is taken from
+    # https://github.com/python/cpython/blob/v3.11.2/Lib/html/parser.py#L300
+    def parse_starttag(self, i):
+        self.__starttag_text = None
+        endpos = self.check_for_whole_start_tag(i)
+        if endpos < 0:
+            return endpos
+        rawdata = self.rawdata
+        self.__starttag_text = rawdata[i:endpos]
+
+        # Now parse the data between i+1 and j into a tag and attrs
+        attrs = []
+        match = tagfind_tolerant.match(rawdata, i+1)
+        assert match, 'unexpected call to parse_starttag()'
+        k = match.end()
+        self.lasttag = tag = match.group(1).lower()
+        while k < endpos:
+            m = attrfind_tolerant.match(rawdata, k)
+            if not m:
+                break
+            attrname, rest, attrvalue = m.group(1, 2, 3)
+            if not rest:
+                attrvalue = None
+            elif attrvalue[:1] == '\'' == attrvalue[-1:] or \
+                 attrvalue[:1] == '"' == attrvalue[-1:]:  # NOQA: Q003,E127
+                attrvalue = attrvalue[1:-1]
+            if attrvalue:
+                attrvalue = unescape(attrvalue)
+            attrs.append((attrname.lower(), attrvalue))
+            k = m.end()
+
+        end = rawdata[k:endpos].strip()
+        if end not in (">", "/>"):  # NOQA: Q000
+            self.handle_data(rawdata[i:endpos])
+            return endpos
+        if end.endswith('/>'):
+            # XHTML-style empty tag: <span attr="value" />
+            self.handle_startendtag(tag, attrs)
+        else:
+            self.handle_starttag(tag, attrs)
+            if tag in self.CDATA_CONTENT_ELEMENTS:
+                self.set_cdata_mode(tag)
+        return endpos
 
     @classmethod
     def _setup_node_classes_cache(cls):
