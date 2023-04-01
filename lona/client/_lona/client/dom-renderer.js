lona-web-org/lona

commit b5d0c5ecc2a50f0cdefdd28340bf48807f472974

client: use new widget API implementation of client2
This patch updates the client to use the widget API implementation of client2,
in order to be compatible with the old and the new widget API, until Lona 2
gets released.

Signed-off-by: Florian Scherf <mail@florianscherf.de>

lona/client/_lona/client/dom-renderer.js
@@ -22,7 +22,7 @@ SOFTWARE.
 
 */
 
-import { LonaWindowShim } from './window-shim.js';
+import { Widget } from '../client2/widget.js';
 import { Lona } from './lona.js';
 
 
@@ -123,18 +123,16 @@ export class LonaDomRenderer {
                     throw(`RuntimeError: unknown widget name '${widget_class_name}'`);
                 }
 
-                var widget_class = Lona.widget_classes[widget_class_name];
-
-                var window_shim = new LonaWindowShim(
+                const widget = new Widget(
                     this.lona_context,
                     this.lona_window,
+                    node,
                     node_id,
+                    Lona.widget_classes[widget_class_name],
+                    widget_data,
                 );
 
-                var widget = new widget_class(window_shim);
-
                 this.lona_window._widgets[node_id] = widget;
-                this.lona_window._widget_data[node_id] = widget_data;
                 this.lona_window._widgets_to_setup.splice(0, 0, node_id);
             }
 
@@ -179,18 +177,16 @@ export class LonaDomRenderer {
 
             // setup widget
             if(node_widget_class_name in Lona.widget_classes) {
-                var widget_class = Lona.widget_classes[node_widget_class_name];
-
-                var window_shim = new LonaWindowShim(
+                const widget = new Widget(
                     this.lona_context,
                     this.lona_window,
+                    node,
                     node_id,
+                    Lona.widget_classes[widget_class_name],
+                    widget_data,
                 );
 
-                var widget = new widget_class(window_shim);
-
                 this.lona_window._widgets[node_id] = widget;
-                this.lona_window._widget_data[node_id] = widget_data;
                 this.lona_window._widgets_to_setup.splice(0, 0, node_id);
             };
         };

lona/client/_lona/client/widget-data-updater.js
@@ -40,9 +40,11 @@ export class LonaWidgetDataUpdater {
         var key_path = payload[0];
         var data = payload.splice(1);
 
+        const widget = this.lona_window._widgets[node_id];
+
         // key path
         var parent_data = undefined;
-        var widget_data = this.lona_window._widget_data[node_id];
+        let widget_data = widget.raw_widget_data;
 
         key_path.forEach(function(key) {
             parent_data = widget_data;
@@ -56,7 +58,7 @@ export class LonaWidgetDataUpdater {
         // RESET
         } else if(operation == Lona.protocol.OPERATION.RESET) {
             if(parent_data === undefined) {
-                this.lona_window._widget_data[node_id] = data[0];
+                widget.raw_widget_data = data[0];
 
             } else {
                 parent_data = data[0];
@@ -74,7 +76,7 @@ export class LonaWidgetDataUpdater {
             };
 
             if(parent_data === undefined) {
-                this.lona_window._widget_data[node_id] = new_data;
+                widget.raw_widget_data = new_data;
 
             } else {
                 parent_data[key_path[key_path.length-1]] = new_data;

lona/client/_lona/client/window.js
@@ -25,7 +25,7 @@ SOFTWARE.
 import { LonaWidgetDataUpdater } from './widget-data-updater.js'
 import { LonaInputEventHandler } from './input-events.js';
 import { LonaDomRenderer } from './dom-renderer.js';
-import { LonaDomUpdater } from './dom-updater.js'
+import { LonaDomUpdater } from './dom-updater.js';
 import { Lona } from './lona.js'
 
 
@@ -120,9 +120,7 @@ export class LonaWindow {
     _clear_node_cache() {
         // running widget deconstructors
         for(var key in this._widgets) {
-            if(this._widgets[key].deconstruct !== undefined) {
-                this._widgets[key].deconstruct();
-            };
+            this._widgets[key].destroy_widget();
         };
 
         // resetting node state
@@ -159,9 +157,7 @@ export class LonaWindow {
             if(key in this._widgets) {
 
                 // run deconstructor
-                if(this._widgets[key].deconstruct !== undefined) {
-                    this._widgets[key].deconstruct();
-                };
+                this._widgets[key].destroy_widget();
 
                 // remove widget
                 delete this._widgets[key];
@@ -188,37 +184,24 @@ export class LonaWindow {
     _run_widget_hooks() {
         // setup
         this._widgets_to_setup.forEach(node_id => {
-            var widget = this._widgets[node_id];
-            var widget_data = this._widget_data[node_id];
-
-            widget.data = JSON.parse(JSON.stringify(widget_data));
-
-            if(widget === undefined) {
+            if(this._widgets[node_id] === undefined) {
                 return;
-            };
+            }
 
-            widget.nodes = this._dom_updater._get_widget_nodes(node_id);
-            widget.root_node = widget.nodes[0];
+            const widget = this._widgets[node_id];
 
-            if(widget.setup !== undefined) {
-                widget.setup();
-            };
+            widget.initialize_widget();
         });
 
         // data_updated
         this._widgets_to_update.forEach(node_id => {
-            var widget = this._widgets[node_id];
-            var widget_data = this._widget_data[node_id];
-
-            widget.data = JSON.parse(JSON.stringify(widget_data));
-
-            if(widget === undefined) {
+            if(this._widgets[node_id] === undefined) {
                 return;
-            };
+            }
 
-            if(widget.data_updated !== undefined) {
-                widget.data_updated();
-            };
+            const widget = this._widgets[node_id];
+
+            widget.run_data_updated_hook();
         });
 
         this._widgets_to_setup = [];

commit 9a047cb87c3cf298b47d08a733238c57e3710f13

client: fix backwards compatibility with legacy frontend widget API
b5d0c5ecc2a5 ("client: use new widget API implementation of client2") broke
backwards compatibility with the legacy frontend widget API, which is still in
use in libraries like lona-bootstrap-5.

Signed-off-by: Florian Scherf <mail@florianscherf.de>

lona/client/_lona/client/dom-renderer.js
@@ -183,9 +183,9 @@ export class LonaDomRenderer {
                 const widget = new Widget(
                     this.lona_context,
                     this.lona_window,
-                    node,
+                    node_list.slice(1, -1).flat(),
                     node_id,
-                    Lona.widget_classes[widget_class_name],
+                    Lona.widget_classes[node_widget_class_name],
                     widget_data,
                 );
 

lona/client2/_lona/client2/widget.js
@@ -73,6 +73,18 @@ export class Widget {
         this.widget_data = new WidgetData(this);
 
         this.widget_object = undefined;
+
+        // legacy API ---------------------------------------------------------
+        // TODO: remove in 2.0
+        this.nodes = [];
+
+        if(Array.isArray(this.root_node)) {
+            this.nodes = this.root_node;
+            this.root_node = this.root_node[0];
+
+        } else {
+            this.nodes = [this.root_node];
+        }
     }
 
     initialize_widget() {
@@ -89,7 +101,7 @@ export class Widget {
 
         // set nodes
         this.widget_object.root_node = this.root_node;
-        this.widget_object.nodes = [this.root_node];
+        this.widget_object.nodes = this.nodes;
 
         // legacy setup hook
         if(this.widget_object.setup !== undefined) {